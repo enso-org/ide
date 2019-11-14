afc12768ff0a71af6a11886d79e8f0a6323ba42a
Adam Obuchowicz <adam.obuchowicz@luna-lang.org>
2019-11-14 10:20:44 +0100

    Downloading msdfgen_wasm.js in build.rs
    To avoid keeping a large binary files in repo, the
    msdfgen library is downloaded from github during build

---
+++ lib/core/msdf-sys/build.rs
@@ -0,0 +1,39 @@
+extern crate download_lp;
+
+mod msdfgen_wasm {
+
+    pub const VERSION  : &str = "v1.0";
+    pub const FILENAME : &str = "msdfgen_wasm.js";
+
+    pub fn download() {
+        let url = format!(
+            "https://github.com/luna/msdf/releases/download/{version}/{filename}",
+            version = VERSION,
+            filename = FILENAME
+        );
+
+        if std::path::Path::new(FILENAME).exists() {
+            std::fs::remove_file(FILENAME).unwrap();
+        }
+
+        // Note [Downloading to src dir]
+        download_lp::download(url.as_str(), ".").unwrap();
+    }
+}
+
+/* Note [Downloading to src dir]
+ * In theory, build.rs scripts should create and modify files in OUT_DIR only, but I haven't found
+ * any way to make #[wasm_bindgen(module="")] taking a file from OUT_DIR (except by providing a full
+ * system path, which is obviously awful)
+ *
+ * Thanks for your understanding
+ *
+ * If you find and implement a better way to downloading js snippets, please remember to remove
+ * msdfgen_wasm.js entry from .gitignore
+ */
+
+fn main() {
+    msdfgen_wasm::download();
+    println!("cargo:rerun-if-changed=build.rs");
+}
+
