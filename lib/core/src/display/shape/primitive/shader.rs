bf450dd10b5da71eb300a8b1aa415147274aa8e3
Wojciech Danilo <wojciech.danilo@gmail.com>
2020-01-02 00:36:34 +0100

    Refactoring

--- lib/core/src/display/shape/primitive/shader.rs
+++ lib/core/src/display/shape/primitive/shader.rs
@@ -22,56 +22,43 @@ fn mk_shape_name <S:Str> (s:S) -> String { format!("shape_{}" , s.as_ref()) }
 //defCdC = Color.rgb [1,0,0,1]
 //defCd  = "rgb2lch(#{GLSL.toCode defCdC})"
 
-// ==================
-// === SDF Canvas ===
-// ==================
-
-#[derive(Clone,Debug)]
-pub struct CanvasShape {
-    shape_num : usize,
-    ids       : Vec<usize>,
-    name      : String,
-}
-
-impl CanvasShape {
-    pub fn new(shape_num:usize) -> Self {
-        let ids  = default();
-        let name = mk_shape_name(shape_num.to_string());
-        Self {shape_num,ids,name}
-    }
-
-    pub fn add_id(&mut self, id:usize) {
-        self.ids.push(id);
-    }
-
-    pub fn add_ids(&mut self, ids:&Vec<usize>) {
-        self.ids.extend(ids)
-    }
-}
 
 
-enum IdGenerationType {
+// ================
+// === GlslItem ===
+// ================
 
-}
+/// Trait describing all types which can be converted to GLSL expressions.
+///
+/// Please note that conversion from string is defined, allowing dirty injection of GLSL code
+/// easily. For example, when moving a shape, you can write `s1.translate("a","b")`, where `a` and
+/// `b` refer to variables defined in the GLSL shader. Such operation is not checked during
+/// compilation, so be careful when using it, please.
 
-pub trait Glsl {
+pub trait GlslItem {
+    /// Checks if the value is zero.
     fn is_zero (&self) -> bool;
-    fn glsl    (&self) -> String;
+
+    /// Converts the value to GLSL code.
+    fn to_glsl (&self) -> String;
 }
 
-impl Glsl for str {
+
+// === Instances ===
+
+impl GlslItem for str {
     fn is_zero (&self) -> bool   { self == "0" || self == "0.0" }
-    fn glsl    (&self) -> String { self.into() }
+    fn to_glsl (&self) -> String { self.into() }
 }
 
-impl Glsl for &str {
+impl GlslItem for &str {
     fn is_zero (&self) -> bool   { (*self).is_zero() }
-    fn glsl    (&self) -> String { (*self).glsl()    }
+    fn to_glsl (&self) -> String { (*self).to_glsl()    }
 }
 
-impl Glsl for f32 {
+impl GlslItem for f32 {
     fn is_zero (&self) -> bool   { *self == 0.0 }
-    fn glsl    (&self) -> String {
+    fn to_glsl (&self) -> String {
         let is_int = self.fract() == 0.0;
         if is_int { iformat!("{self}.0") }
         else      { iformat!("{self}")   }
@@ -80,103 +67,140 @@ impl Glsl for f32 {
 
 
 
+// ===================
+// === CanvasShape ===
+// ===================
+
+/// Reference to a shape defined on `Canvas`.
+#[derive(Clone,Debug)]
+pub struct CanvasShape {
+    shape_num : usize,
+    ids       : Vec<usize>,
+    name      : String,
+}
+
+impl CanvasShape {
+    /// Constructor.
+    pub fn new(shape_num:usize) -> Self {
+        let ids  = default();
+        let name = mk_shape_name(shape_num.to_string());
+        Self {shape_num,ids,name}
+    }
+
+    /// Adds new id enclosed in this shape.
+    pub fn add_id(&mut self, id:usize) {
+        self.ids.push(id);
+    }
+
+    /// Add multiple ids enclosed in this shape.
+    pub fn add_ids(&mut self, ids:&Vec<usize>) {
+        self.ids.extend(ids)
+    }
+}
+
+
+
+// ==============
+// === Canvas ===
+// ==============
+
+// === Definition ===
+
+/// Canvas for drawing vector graphics.
+///
+/// The API is stateful, similar to the API of HTML5 canvas element.
+/// It uses GLSL and signed distance fields under the hood.
+
 #[derive(Debug,Default)]
 pub struct Canvas {
     shape_num  : usize,
     last_id    : usize,
-    bb_lines   : Vec<String>,
     code_lines : Vec<String>,
 }
 
+
+// === ID Management ===
+
 impl Canvas {
-    pub fn get_new_id(&mut self) -> usize {
+    /// Generates a new unique shape's ID.
+    fn get_new_id(&mut self) -> usize {
         let id = self.last_id;
         self.last_id += 1;
         id
     }
 
-    pub fn add_code_line(&mut self, line:String) {
-        self.code_lines.push(line);
-    }
-
-    pub fn add_code_line_ind(&mut self, line:String) {
-        self.add_code_line(format!("    {}",line));
-    }
-
-    pub fn define<E:Str>(&mut self, ty:&str, name:&str, expr:E) {
-        let max_type_length = 7;
-        let max_name_length = 13;
-        let ty              = format!("{:1$}" , ty   , max_type_length);
-        let name            = format!("{:1$}" , name , max_name_length);
-        self.add_code_line_ind(iformat!("{ty} {name} = {expr.as_ref()};"));
-    }
-
-    pub fn add_bb_line(&mut self, line:String) {
-        self.bb_lines.push(line);
-    }
-
-    pub fn gen_new_color_id(&mut self, name:&str) -> usize {
-        let id       = self.get_new_id();
-        let id_name  = mk_id_name(name);
-        let sdf_name = mk_sdf_name(name);
-        self.define("id",&id_name,iformat!("new_id_layer({sdf_name},{id})"));
-        id
-    }
-
-    pub fn merge_id_layers(&mut self, a:CanvasShape, b:CanvasShape, name:String) {
-        let id_name = mk_id_name(&name);
-        self.define("id",&id_name,iformat!("id_union({a.name},{b.name},{a.name}.id,{b.name}.id)"));
-    }
-
-    pub fn intersect_id_layers(&mut self, a:CanvasShape, b:CanvasShape, name:String) {
-        let id_name = mk_id_name(&name);
-        self.define("id",&id_name,iformat!("id_intersection({a.name},{b.name},{a.name}.id)"));
-    }
-
-    pub fn diff_id_layers(&mut self, a:CanvasShape, b:CanvasShape, name:String) {
-        let id_name = mk_id_name(&name);
-        self.define("id",&id_name,iformat!("id_difference({a.name},{b.name},{a.name}.id)"));
-    }
-
-    pub fn keep_id_layer(&mut self, a:CanvasShape, name:String) {
-        let id_name = mk_id_name(&name);
-        self.define("id",&id_name,iformat!("{a.name}.id"));
-    }
-
-    pub fn code(&self) -> String {
-        self.code_lines.join("\n")
-    }
-
+    /// Generate a new unique shape number.
     pub fn get_new_shape_num(&mut self) -> usize {
         let out = self.shape_num;
         self.shape_num += 1;
         out
     }
+}
 
-    pub fn register_shape(&mut self) -> CanvasShape {
+
+// === GLSL Modification ===
+
+impl Canvas {
+    /// Adds new code line to the GLSL code.
+    fn add_code_line(&mut self, line:String) {
+        self.code_lines.push(line);
+    }
+
+    /// Adds new indented code line to the GLSL code.
+    pub fn add_indented_code_line(&mut self, line:String) {
+        self.add_code_line(format!("    {}",line));
+    }
+
+    /// Defines a new variable in the GLSL code.
+    fn define<E:Str>(&mut self, ty:&str, name:&str, expr:E) {
+        let max_type_length = 7;
+        let max_name_length = 13;
+        let ty              = format!("{:1$}" , ty   , max_type_length);
+        let name            = format!("{:1$}" , name , max_name_length);
+        self.add_indented_code_line(iformat!("{ty} {name} = {expr.as_ref()};"));
+    }
+
+    /// Get the final GLSL code.
+    fn to_glsl(&self) -> String {
+        self.code_lines.join("\n")
+    }
+}
+
+
+// === Shape Modification ===
+
+impl Canvas {
+    /// Creates a new `CanvasShape` object. The shape is not assigned with any id and is not
+    /// represented in the GLSL code yet.
+    fn new_canvas_shape(&mut self) -> CanvasShape {
         let num = self.get_new_shape_num();
         CanvasShape::new(num)
     }
 
-    pub fn new_shape(&mut self, sdf:&str, cd:Option<&str>) -> CanvasShape {
+    /// Defines a new shape with a new id and associated parameters, like color.
+    fn define_shape(&mut self, sdf:&str, cd:Option<&str>) -> CanvasShape {
         let color     = "rgb2lch(vec3(1.0,0.0,0.0)";
-        let mut shape = self.register_shape();
+        let mut shape = self.new_canvas_shape();
+        let id        = self.get_new_id();
         let id_name   = mk_id_name  (&shape.name);
         let cd_name   = mk_cd_name  (&shape.name);
         let sdf_name  = mk_sdf_name (&shape.name);
         self.define("color" , &cd_name    , iformat!("{color}"));
         self.define("sdf"   , &sdf_name   , iformat!("{sdf}"));
-        shape.add_id(self.gen_new_color_id(&shape.name));
-        self.define("shape", &shape.name, iformat!("shape({id_name},{cd_name},{sdf_name})"));
+        self.define("id"    , &id_name    , iformat!("new_id_layer({sdf_name},{id})"));
+        self.define("shape" , &shape.name , iformat!("shape({id_name},{cd_name},{sdf_name})"));
+        shape.add_id(id);
         shape
     }
 
+    /// Define a new shape from the provided GLSL expression.
     pub fn new_shape_from_expr(&mut self, expr:&str) -> CanvasShape {
-        let shape = self.register_shape();
+        let shape = self.new_canvas_shape();
         self.define("shape",&shape.name,expr);
         shape
     }
 
+    /// Create a union shape from the provided shape components.
     pub fn union(&mut self, s1:CanvasShape, s2:CanvasShape) -> CanvasShape {
         let mut shape = self.new_shape_from_expr(&iformat!("union({s1.name},{s2.name})"));
         shape.add_ids(&s1.ids);
@@ -184,26 +208,126 @@ impl Canvas {
         shape
     }
 
-
+    /// Translate the current canvas origin.
     pub fn translate(&mut self, x:f32, y:f32) {
-        self.add_code_line_ind(iformat!("position = sdf_translate(position, vec2({x.glsl()},{y.glsl()}));"));
+        let expr = iformat!("sdf_translate(position, vec2({x.to_glsl()},{y.to_glsl()}))");
+        self.define("","position",expr);
     }
 
 }
 
 
+// =================
+// === PrimShape ===
+// =================
 
-pub trait SdfShape {
-    fn sdf_code(&self) -> String;
+/// Class of primitive shapes. Primitive shapes are described by a SDF field.
+pub trait PrimShape {
+    fn to_sdf_code(&self) -> String;
 }
 
-macro_rules! shapes {
+
+// ===============================
+// === Shape Definition Macros ===
+// ===============================
+
+/// Defines primitive shapes and appropriate shape wrappers.
+///
+/// Primitive shapes are defined in the `prim_shapes` module, while the shape wrappers are placed in
+/// the `shapes` module. The shape definition accepted by this macro is similar to both a struct and
+/// a function definition. It's body should be defined as a valid GLSL code.
+///
+/// For the following input:
+/// ```
+/// define_shapes! {
+///     Circle (radius:f32) {
+///         return sdf(length(position)-radius, bbox_center(radius,radius));
+///     }
+/// ```
+///
+/// The following output will be generated:
+/// ```
+/// pub mod prim_shapes {
+///     use super::*;
+///
+///     #[derive(Debug,Clone)]
+///     pub struct Circle {
+///         pub glsl_name : String,
+///         pub radius    : String,
+///     }
+///
+///     impl Circle {
+///         pub fn new<radius:GlslItem>(radius:radius) -> Self {
+///             let glsl_name = "circle".to_string();
+///             let radius    = radius.to_glsl();
+///             Self {glsl_name,radius}
+///         }
+///
+///         pub fn draw(&self, canvas:&mut Canvas) -> CanvasShape {
+///             let args = vec!["position".to_string(), self.radius.to_glsl()].join(",");
+///             let code = format!("{}({})",self.glsl_name,args);
+///             canvas.define_shape(&code,None)
+///         }
+///     }
+///
+///     impl PrimShape for Circle {
+///            fn to_sdf_code(&self) -> String {
+///                let body = "return sdf(length(position)-radius, bbox_center(radius,radius));";
+///                let args = vec![
+///                    "vec2 position".to_string(),
+///                    format!("{} {}", <$f32 as Item>::gpu_type_name(), "radius")
+///                    ].join(", ");
+///                format!("sdf {} ({}) {{ {} }}",self.glsl_name,args,body)
+///            }
+///        }
+///
+///        impl ShapeDef for Circle {
+///            fn render_glsl(&self, renderer:&mut GlslRenderer) -> CanvasShape {
+///                self.draw(&mut renderer.canvas)
+///            }
+///        }
+/// }
+///
+/// pub mod shapes {
+///     use super::*;
+///
+///     pub type Circle = Immutable<prim_shapes::Circle>;
+///     pub fn Circle<radius:GlslItem>(radius:radius) -> Circle {
+///         Shape::new(prim_shapes::Circle::new(radius))
+///     }
+/// }
+/// ```
+
+macro_rules! define_shapes {
     ( $($name:ident $args:tt $body:tt)* ) => {
-        $(shape! {$name $args $body} )*
+        pub mod prim_shapes {
+            use super::*;
+            $(define_prim_shape! {$name $args $body} )*
+        }
+
+        pub mod shapes {
+            use super::*;
+            $(define_shape_wrappers! {$name $args $body} )*
+        }
     };
 }
 
-macro_rules! shape {
+/// See the docs of `define_shapes`.
+macro_rules! define_shape_wrappers {
+    ( $name:ident ( $($field:ident : $field_type:ty),* $(,)? ) { $($code:tt)* } ) => {
+
+        /// Smart shape type.
+        pub type $name = Immutable<prim_shapes::$name>;
+
+        /// Smart shape constructor.
+        pub fn $name <$($field:GlslItem),*> ( $($field : $field),* ) -> $name {
+            Immutable::new(prim_shapes::$name::new($($field),*))
+        }
+    }
+}
+
+/// See the docs of `define_shapes`.
+macro_rules! define_prim_shape {
     ( $name:ident ( $($field:ident : $field_type:ty),* $(,)? ) { $($code:tt)* } ) => {
         /// The shape definition.
         #[derive(Debug,Clone)]
@@ -212,33 +336,25 @@ macro_rules! shape {
             $(pub $field  : String),*
         }
 
-        /// Smart shape constructor.
-
-
         impl $name {
             /// Constructor.
-            pub fn new <$($field:Glsl),*> ( $($field : $field),* ) -> Self {
+            pub fn new <$($field:GlslItem),*> ( $($field : $field),* ) -> Self {
                 let glsl_name = stringify!($name).to_snake_case();
-                $(let $field = $field.glsl();)*
+                $(let $field = $field.to_glsl();)*
                 Self {glsl_name,$($field),*}
             }
-
-            pub fn new_shape <$($field:Glsl),*> ( $($field : $field),* ) -> Shape<Self> {
-                Shape::new(Self::new($($field),*))
-            }
-
 
             /// Draws the shape on the provided canvas. Shapes are always drawn in the center of
             /// the canvas. In order to move them somewhere, use the canvas moving API.
             pub fn draw(&self, canvas:&mut Canvas) -> CanvasShape {
-                let args = vec!["position".to_string(), $(self.$field.glsl()),* ].join(",");
+                let args = vec!["position".to_string(), $(self.$field.to_glsl()),* ].join(",");
                 let code = format!("{}({})",self.glsl_name,args);
-                canvas.new_shape(&code,None)
+                canvas.define_shape(&code,None)
             }
         }
 
-        impl SdfShape for $name {
-            fn sdf_code(&self) -> String {
+        impl PrimShape for $name {
+            fn to_sdf_code(&self) -> String {
                 let body = stringify!($($code)*);
                 let args = vec!["vec2 position".to_string(), $(
                     format!("{} {}", <$field_type as Item>::gpu_type_name(), stringify!($field))
@@ -256,28 +372,49 @@ macro_rules! shape {
 }
 
 
+
+// =============
+// === HasId ===
+// =============
+
+/// Each shape definition has to be assigned with an unique id in order for the renderer to
+/// implement results cache. For example, we can create a circle as `s1` and then move it right,
+/// which will result in the `s2` object. We can merge them together creating `s3` object. The
+/// renderer needs to discover that `s3` was in fact created from two `s1` under the hood.
+///
+/// This trait should not be implemented manually. It is implemented by `Immutable`, which wraps
+/// every shape definition.
+pub trait HasId {
+    fn id(&self) -> usize;
+}
+
+
+
+// =================
+// === Immutable ===
+// =================
+
+/// Wrapper for primitive shapes. It makes them both immutable as well as assigns each shape with
+/// an unique id.
 #[derive(Debug,Derivative,Shrinkwrap)]
 #[derivative(Clone(bound=""))]
-pub struct Shape<T> {
+pub struct Immutable<T> {
     rc:Rc<T>
 }
 
-impl<T> Shape<T> {
+impl<T> Immutable<T> {
     pub fn new(t:T) -> Self {
         Self {rc:Rc::new(t)}
     }
 }
 
-impl<T> HasId for Shape<T> {
+impl<T> HasId for Immutable<T> {
     fn id(&self) -> usize {
         Rc::downgrade(&self.rc).as_raw() as *const() as usize
     }
 }
 
 
-trait HasId {
-    fn id(&self) -> usize;
-}
 
 trait ShapeDef {
     fn render_glsl(&self, renderer:&mut GlslRenderer) -> CanvasShape;
@@ -285,7 +422,7 @@ trait ShapeDef {
 
 trait IsShape = ShapeDef + HasId;
 
-impl<T:ShapeDef> ShapeDef for Shape<T> {
+impl<T:ShapeDef> ShapeDef for Immutable<T> {
     fn render_glsl(&self, renderer:&mut GlslRenderer) -> CanvasShape {
         self.rc.render_glsl(renderer)
     }
@@ -361,9 +498,9 @@ impl GlslRenderer {
         let old_ctx = self.tx_ctx;
         let new_ctx = self.get_new_tx_ctx();
         self.tx_ctx = new_ctx;
-        self.canvas.add_code_line_ind(iformat!("vec2 position_{new_ctx} = position;"));
+        self.canvas.define("vec2",&iformat!("position_{new_ctx}"),"position");
         let out = f(self);
-        self.canvas.add_code_line_ind(iformat!("position = position_{new_ctx};"));
+        self.canvas.define("","position",iformat!("position_{new_ctx}"));
         self.tx_ctx = old_ctx;
         out
     }
@@ -383,28 +520,28 @@ impl GlslRenderer {
 
     pub fn render<S:IsShape>(&mut self, shape:&S) -> String {
         let canvas_shape = self.render_shape(shape);
-        iformat!("shape main(vec2 position) {{\n{self.canvas.code()}\n    return {canvas_shape.name};\n}}")
+        iformat!("shape main(vec2 position) {{\n{self.canvas.to_glsl()}\n    return {canvas_shape.name};\n}}")
     }
 }
 
 
 pub trait ShapeOps
 where Self:Sized+Clone {
-    fn translate(&self,x:f32,y:f32) -> Shape<Translate<Self>> {
-        Shape::new(Translate::new(self,x,y))
+    fn translate(&self,x:f32,y:f32) -> Immutable<Translate<Self>> {
+        Immutable::new(Translate::new(self,x,y))
     }
 
-    fn union<S:Clone>(&self,that:S) -> Shape<Union<Self,S>> {
-        Shape::new(Union::new(self,&that))
+    fn union<S:Clone>(&self,that:S) -> Immutable<Union<Self,S>> {
+        Immutable::new(Union::new(self,&that))
     }
 }
 
-impl<T> ShapeOps for Shape<T> {}
+impl<T> ShapeOps for Immutable<T> {}
 
 
 
-impl<T,S:Clone> std::ops::Add<S> for Shape<T> {
-    type Output = Shape<Union<Shape<T>,S>>;
+impl<T,S:Clone> std::ops::Add<S> for Immutable<T> {
+    type Output = Immutable<Union<Immutable<T>,S>>;
     fn add(self, that:S) -> Self::Output {
         self.union(that)
     }
@@ -413,7 +550,7 @@ impl<T,S:Clone> std::ops::Add<S> for Shape<T> {
 
 
 
-shapes! {
+define_shapes! {
 
     // === Infinite ===
 
@@ -497,6 +634,8 @@ shapes! {
 
 
 pub fn main() {
+    use shapes::*;
+
     let mut r:GlslRenderer = default();
     let canvas = &mut r.canvas;
 
@@ -506,14 +645,14 @@ pub fn main() {
 //    let s2 = c2.draw(canvas);
 //    canvas.union(s1,s2);
 
-    let s1 = Circle::new_shape(10.0);
+    let s1 = Circle(10.0);
     let s2 = s1.translate(1.0,2.0);
     let s3 = s1 + s2;
 
     println!("{}", r.render(&s3));
 
 //
-//    println!("{}", c1.sdf_code());
+//    println!("{}", c1.to_sdf_code());
 }
 
 
