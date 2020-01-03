8a4d3180543bc9a1dafbf44ae2161c54fc3b3a04
Danilo Guanabara <danguafer@gmail.com>
2020-01-03 20:18:38 -0300

    Removed debug

--- lib/core/src/control/mouse_manager.rs
+++ lib/core/src/control/mouse_manager.rs
@@ -159,7 +159,6 @@ impl MouseWheelEvent {
         let movement_x      = event.delta_x() as f32;
         let movement_y      = event.delta_y() as f32;
         let is_ctrl_pressed = event.ctrl_key();
-        println!("{}x{}", movement_x, movement_y);
         Self { movement_x,movement_y,is_ctrl_pressed }
     }
 }
