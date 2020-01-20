b7ebee09926f3eb025006aa662462499cfd57fa5
Wojciech Danilo <wojciech.danilo@gmail.com>
2020-01-20 07:01:58 +0100

    Refactoring

--- lib/core/src/display/world.rs
+++ lib/core/src/display/world.rs
@@ -185,7 +185,7 @@ impl RenderPassRunner {
 
     pub fn run(&mut self, context:&Context) {
         self.context.bind_framebuffer(Context::FRAMEBUFFER,self.framebuffer.as_ref());
-        self.pass.run(context);
+        self.pass.run(context,&self.variables);
     }
 
     fn initialize(&mut self) {
@@ -232,7 +232,7 @@ impl RenderPassOutput {
 
 
 pub trait RenderPass : 'static {
-    fn run(&mut self, context:&Context);
+    fn run(&mut self, context:&Context, variables:&UniformScope);
     fn outputs(&self) -> Vec<RenderPassOutput> {
         default()
     }
@@ -254,7 +254,7 @@ impl WorldRenderPass {
 }
 
 impl RenderPass for WorldRenderPass {
-    fn run(&mut self, context:&Context) {
+    fn run(&mut self, context:&Context, _:&UniformScope) {
         context.clear_color(0.0, 0.0, 0.0, 1.0);
         context.clear(Context::COLOR_BUFFER_BIT);
         self.target.render();
@@ -281,7 +281,7 @@ impl ScreenRenderPass {
 }
 
 impl RenderPass for ScreenRenderPass {
-    fn run(&mut self, _:&Context) {
+    fn run(&mut self, _:&Context, _:&UniformScope) {
         self.screen.render();
     }
 }
@@ -289,12 +289,13 @@ impl RenderPass for ScreenRenderPass {
 
 #[derive(Debug)]
 pub struct PixelReadPassData {
-    buffer: WebGlBuffer,
+    uniform : Uniform<Vector4<i32>>,
+    buffer  : WebGlBuffer,
 }
 
 impl PixelReadPassData {
-    pub fn new(buffer:WebGlBuffer) -> Self {
-        Self {buffer}
+    pub fn new(uniform:Uniform<Vector4<i32>>, buffer:WebGlBuffer) -> Self {
+        Self {uniform,buffer}
     }
 }
 
@@ -302,56 +303,79 @@ impl PixelReadPassData {
 #[derive(Debug,Default)]
 struct PixelReadPass {
     data: Option<PixelReadPassData>,
+    sync: Option<WebGlSync>,
 }
 
 impl PixelReadPass {
     pub fn new() -> Self {
         default()
     }
-}
-
-impl RenderPass for PixelReadPass {
-    fn run(&mut self, context:&Context) {
-
-        let data = match &self.data {
-            Some(data) => data,
-            None => {
-                let buffer     = context.create_buffer().unwrap();
-                context.bind_buffer(Context::PIXEL_PACK_BUFFER,Some(&buffer));
-                let array      = ArrayBuffer::new(4);
-                let array_view = Some(&array);
-                let target     = Context::PIXEL_PACK_BUFFER;
-                let usage      = Context::DYNAMIC_READ;
-                context.buffer_data_with_opt_array_buffer(target,array_view,usage);
-                let data = PixelReadPassData::new(buffer);
-                self.data = Some(data);
-                self.data.as_ref().unwrap()
-            }
-        };
-
-        context.bind_buffer(Context::PIXEL_PACK_BUFFER,Some(&data.buffer));
-
-        // REMOVE ME
-        context.bind_buffer(Context::PIXEL_PACK_BUFFER,None);
 
 
+    fn init_if_fresh(&mut self, context:&Context, variables:&UniformScope) {
+        if self.data.is_none() {
+            let buffer  = context.create_buffer().unwrap();
+            let array   = ArrayBuffer::new(4);
+            let target  = Context::PIXEL_PACK_BUFFER;
+            let usage   = Context::DYNAMIC_READ;
+            let uniform = variables.add_or_panic("pass_pixel_color",Vector4::new(0,0,0,0));
+            context.bind_buffer(target,Some(&buffer));
+            context.buffer_data_with_opt_array_buffer(target,Some(&array),usage);
+            self.data = Some(PixelReadPassData::new(uniform,buffer));
+        }
+    }
+
+    fn run_not_synced(&mut self, context:&Context) {
+        let data   = self.data.as_ref().unwrap();
         let mousex = 228*2;
         let mousey = 70*2;
         let width  = 1;
         let height = 1;
         let format = Context::RGBA;
         let typ    = Context::UNSIGNED_BYTE;
+        let target = Context::PIXEL_PACK_BUFFER;
         let offset = 0;
-        let mut dst: Vec<u8> = vec![0,0,0,0];
-        context.read_pixels_with_u8_array_and_dst_offset(mousex,mousey,width,height,format,typ,&mut dst,offset);
+        context.bind_buffer(target,Some(&data.buffer));
+        context.read_pixels_with_i32(mousex,mousey,width,height,format,typ,offset);
+        let condition = Context::SYNC_GPU_COMMANDS_COMPLETE;
+        let flags     = 0;
+        let sync      = context.fence_sync(condition,0).unwrap();
+        self.sync     = Some(sync);
+        context.flush();
+    }
 
-        println!("GOT: {:?}", dst);
+    fn check_and_handle_sync(&mut self, context:&Context, sync:&WebGlSync) {
+        let data   = self.data.as_ref().unwrap();
+        let status = context.get_sync_parameter(sync,Context::SYNC_STATUS);
+        if status == Context::SIGNALED {
+            context.delete_sync(Some(sync));
+            self.sync      = None;
+            let target     = Context::PIXEL_PACK_BUFFER;
+            let offset     = 0;
+            let mut result = vec![0,0,0,0];
+            context.bind_buffer(target,Some(&data.buffer));
+            context.get_buffer_sub_data_with_i32_and_u8_array(target,offset,&mut result);
+            data.uniform.set(Vector4::from_iterator(result.iter().map(|t| *t as i32)));
+            println!("GOT: {:?}", result);
+        }
+    }
+}
 
+impl RenderPass for PixelReadPass {
+    fn run(&mut self, context:&Context, variables:&UniformScope) {
+        self.init_if_fresh(context,variables);
+        if let Some(sync) = self.sync.clone() {
+            self.check_and_handle_sync(context,&sync);
+        }
+        if self.sync.is_none() {
+            self.run_not_synced(context);
+        }
     }
 }
 
 use js_sys::ArrayBuffer;
 use web_sys::WebGlBuffer;
+use web_sys::WebGlSync;
 
 //pub fn buffer_data_with_opt_array_buffer(
 //    &self,
