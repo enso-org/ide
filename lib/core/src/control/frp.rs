7b693ad0d36fdfe9504f3b870c82e7e54fdc14d1
Wojciech Danilo <wojciech.danilo@gmail.com>
2020-01-24 06:33:52 +0100

    Refactoring

--- lib/core/src/control/frp.rs
+++ lib/core/src/control/frp.rs
@@ -1,4 +1,4 @@
-
+#![allow(missing_docs)]
 
 
 use crate::prelude::*;
@@ -11,20 +11,35 @@ pub struct SwitchData {
 
 
 
-pub struct Event<T> {
-    value: T
-}
-
-
-
 // ============
 // === Node ===
 // ============
 
 
-pub trait Node {
+pub trait HasOutput {
     type Output;
+}
 
+pub struct Node<T> {
+    raw: Rc<dyn HasOutput<Output=T>>,
+}
+
+impl<T> Node<T> {
+    pub fn new<A:HasOutput<Output=T>+'static>(a:A) -> Self {
+        let raw = Rc::new(a);
+        Self {raw}
+    }
+
+    pub fn clone_ref(&self) -> Self {
+        let raw = self.raw.clone();
+        Self {raw}
+    }
+}
+
+impl<A:HasOutput<Output=T>+CloneRef+'static,T> From<&A> for Node<T> {
+    fn from(a:&A) -> Self {
+        Self::new(a.clone_ref())
+    }
 }
 
 
@@ -45,43 +60,72 @@ impl<T> {
         Self {callbacks}
     }
 
-    pub fn get(&self) -> Event<T> {
-        todo!()
+    pub fn emit(&self, value:&T) {
+
     }
 }}
 
-impl<T> Node for EventEmitter<T> {
+impl<T> HasOutput for EventEmitter<T> {
     type Output = T;
 }
 
 
 
-// ===========
-// === Map ===
-// ===========
 
-shared! { Map
 
-pub struct MapData<A,B> {
-    source : Rc<dyn Node<Output=A>>,
-    func   : Rc<dyn Fn(&A) -> B>,
+// ===============
+// === Lambda1 ===
+// ===============
+
+shared! { Lambda1
+
+pub struct LambdaData1<A,T> {
+    source : Node<A>,
+    func   : Rc<dyn Fn(&A) -> T>,
 }
 
-impl<A,B> {
-    pub fn new<F:'static + Fn(&A) -> B, Source:Node<Output=A>+'static>
-    (source:&Source, f:F) -> Self {
-        let source = Rc::new(source.clone_ref());
+impl<A,T> {
+    pub fn new<F:'static + Fn(&A) -> T, Source:Into<Node<A>>>
+    (source:Source, f:F) -> Self {
+        let source = source.into();
         let func   = Rc::new(f);
         Self {source,func}
     }
 }}
 
 
-impl<A,B> Node for Map<A,B> {
-    type Output = B;
+impl<A,T> HasOutput for Lambda1<A,T> {
+    type Output = T;
 }
 
 
+
+// ===============
+// === Lambda2 ===
+// ===============
+
+shared! { Lambda2
+
+pub struct LambdaData2<A,B,T> {
+    source1 : Node<A>,
+    source2 : Node<B>,
+    func    : Rc<dyn Fn(&A,&B) -> T>,
+}
+
+impl<A,B,T> {
+    pub fn new<F:'static + Fn(&A,&B) -> T, Source1:Into<Node<A>>, Source2:Into<Node<B>>>
+    (source1:Source1, source2:Source2, f:F) -> Self {
+        let source1 = source1.into();
+        let source2 = source2.into();
+        let func    = Rc::new(f);
+        Self {source1,source2,func}
+    }
+}}
+
+
+impl<A,B,T> HasOutput for Lambda2<A,B,T> {
+    type Output = T;
+}
 
 
 
@@ -93,7 +137,12 @@ pub fn test () {
 
     let e1 = EventEmitter::<i32>::new();
 
-    let n1 = Map::new(&e1, |i| {i+1});
-//    let n2 = Map::new(&e1, |i| {i+2});
+    let n1 = Lambda1::new(&e1, |i| {i+1});
+    let n2 = Lambda1::new(&e1, |i| {i+1});
+
+    let n3 = Lambda2::new(&n1,&n2,|i,j| {i * j});
+
+
+    e1.emit(&7);
 
 }
