4a2bebd3b9c477ef68c4e9088bd9e4f32f4b10e5
Danilo Guanabara <danguafer@gmail.com>
2020-02-03 20:29:35 -0300

    Moved du -h to assert_max_wasm_size

--- script/assert_max_wasm_size.sh
+++ script/assert_max_wasm_size.sh
@@ -2,8 +2,11 @@
 
 echoerr() { echo "$@" 1>&2; }
 
-current_size=$(cat file_size)
+cd target/web || exit 1
+
+current_size=$(du -h basegl_bg_opt.wasm.gz | awk '{ print $1 }')
 current_size="${current_size::-1}"
+
 max_size=2.1 # MB
 echo "Current size: ${current_size}M. expected maximum size: ${max_size}M"
 if (( $(echo "$current_size <= $max_size" |bc -l) ));
--- script/minimize_wasm.sh
+++ script/minimize_wasm.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-cd target/web
+cd target/web || exit 1
 
 wasm-opt -O3 -o basegl_bg_opt.wasm basegl_bg.wasm
 gzip --best --force basegl_bg_opt.wasm
-du -h basegl_bg_opt.wasm.gz | awk '{ print $1 }'
