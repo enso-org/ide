e8aecefc594e6014d7ffa6a36f65cfebd48d2c5e
Danilo Guanabara <danguafer@gmail.com>
2020-02-04 19:51:43 -0300

    Fixes from reviews

--- script/assert_max_wasm_size.sh
+++ script/assert_max_wasm_size.sh
@@ -3,10 +3,11 @@
 echoerr() { echo "$@" 1>&2; }
 
 current_size=$(cat file_size)
-current_size="${current_size:0:$((${#current_size} - 1))}"
+current_size_len=$((${#current_size} - 1))
+current_size="${current_size:0:current_size_len}"
 
 max_size=2.1 # MB
-echo "Current size: ${current_size}M. expected maximum size: ${max_size}M"
+echo "Current size: ${current_size}MiB. expected maximum size: ${max_size}MiB"
 if (( $(echo "$current_size <= $max_size" |bc -l) ));
 then
   echo OK
--- script/minimize_wasm.sh
+++ script/minimize_wasm.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 cd target/web || exit 1
 
+cmd=$(command -v wasm-opt)
+cmd_len=${#cmd}
+
+if [ $cmd_len -eq 0 ]
+then
+  npm install binaryen
+fi
+
 wasm-opt -O3 -o basegl_bg_opt.wasm basegl_bg.wasm
 gzip --best --force basegl_bg_opt.wasm
 du -h basegl_bg_opt.wasm.gz | awk '{ print $1 }'
