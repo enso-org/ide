2d0693916ae4e9f7b91c179e7dddc95a148d0a65
Danilo Guanabara <danguafer@gmail.com>
2020-02-13 15:44:45 -0300

    Added FIXME note in ide.rs

--- lib/gui/src/ide.rs
+++ lib/gui/src/ide.rs
@@ -11,6 +11,8 @@ use ide::entry_point::entry_point;
 pub fn run_example_ide() {
     web::forward_panic_hook_to_console();
     web::set_stdout();
+
+    // FIXME: This code is temporary. It's used to remove the loader UI.
     basegl_core_msdf_sys::run_once_initialized(|| {
         web::get_element_by_id("loader").map(|t| {
             t.parent_node().map(|p| {
