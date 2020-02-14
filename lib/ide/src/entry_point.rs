0cc5ebea8db963aaf1003de8ea618a33f3a52854
Adam Obuchowicz <adam.obuchowicz@luna-lang.org>
2020-02-14 11:21:23 +0100

    Apply some review fixes

--- lib/ide/src/entry_point.rs
+++ lib/ide/src/entry_point.rs
@@ -1,6 +1,6 @@
 #![allow(missing_docs)]
 
-use super::view::project_view::ProjectView;
+use super::view::project::ProjectView;
 
 pub fn entry_point() {
     ProjectView::new().forget();
--- lib/ide/src/view.rs
+++ lib/ide/src/view.rs
@@ -10,6 +10,6 @@
 #![warn(unused_qualifications)]
 
 pub mod ui_component;
-pub mod project_view;
-pub mod view_layout;
+pub mod project;
+pub mod layout;
 pub mod text_editor;
