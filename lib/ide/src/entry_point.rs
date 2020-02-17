5d20641db2510a4d2466c213f3f2e6ccde4d17dd
Danilo Guanabara <danguafer@gmail.com>
2020-02-17 06:33:38 -0300

    entry_point doc update

--- lib/ide/src/entry_point.rs
+++ lib/ide/src/entry_point.rs
@@ -2,7 +2,7 @@
 
 use super::view::project::ProjectView;
 
-/// This function is the IDE entry point responsible for creating the ProjectView.
+/// This function is the IDE entry point responsible for all views and controllers.
 pub fn entry_point() {
     ProjectView::new().forget();
 }
