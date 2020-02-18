266b87483c6327ab279881680a3f65836368a2c5
Adam Obuchowicz <adam.obuchowicz@luna-lang.org>
2020-02-18 10:41:12 +0100

    Update docs of entry_point

--- lib/ide/src/entry_point.rs
+++ lib/ide/src/entry_point.rs
@@ -2,7 +2,7 @@
 
 use super::view::project::ProjectView;
 
-/// This function is the IDE entry point responsible for all views and controllers.
+/// This function is the IDE entry point responsible for setting up all views and controllers.
 pub fn entry_point() {
     ProjectView::new().forget();
 }
