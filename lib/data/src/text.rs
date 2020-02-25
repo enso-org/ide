9a7aa3564505054fdf2d0e60af951217b7e68a17
Josef Vonasek <j.f.vonasek@gmail.com>
2020-02-25 10:51:18 +0100

    doc comments

--- lib/data/src/text.rs
+++ lib/data/src/text.rs
@@ -1,3 +1,6 @@
+//! Strongly typed container indexing primitives.
+//! Primarily used for text manipulation.
+
 use std::ops::Add;
 use std::ops::Sub;
 use serde::Serialize;
@@ -5,28 +8,37 @@ use serde::Deserialize;
 
 
 
+/// Strongly typed index into container.
+#[allow(missing_docs)]
 #[derive(Clone,Copy,Debug,Default,PartialEq,Eq,PartialOrd,Ord,Serialize,Deserialize)]
 pub struct Index { pub value:usize }
 
+/// Strongly typed size of container.
+#[allow(missing_docs)]
 #[derive(Clone,Copy,Debug,Default,PartialEq,Eq,PartialOrd,Ord,Serialize,Deserialize)]
 pub struct Size { pub value:usize }
 
+/// Strongly typed span into container with index and size.
+#[allow(missing_docs)]
 #[derive(Clone,Copy,Debug,Default,PartialEq,Eq,PartialOrd,Ord,Serialize,Deserialize)]
 pub struct Span { pub index:Index, pub size:Size }
 
 impl Index {
+    /// Initializes Index with given value.
     pub fn new(value:usize) -> Self {
         Index {value}
     }
 }
 
 impl Size {
+    /// Initializes Size with given value.
     pub fn new(value:usize) -> Self {
         Size {value}
     }
 }
 
 impl Span {
+    /// Initializes Span with given values.
     pub fn new(index:Index, size:Size) -> Self {
         Span {index,size}
     }
