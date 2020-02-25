b714ecef6f18b4b044a6f174ee3bf1de9c23aeec
Danilo Guanabara <danguafer@gmail.com>
2020-02-25 01:09:04 -0300

    Comments

--- app/src/main/index.js
+++ app/src/main/index.js
@@ -56,7 +56,6 @@ app.on('window-all-closed', () => {
 })
 
 app.on('activate', () => {
-    // On macOS it is common to re-create a window even after all windows have been closed.
     if (main_window_keep_alive === null) {
         main_window_keep_alive = create_main_window()
     }
