9acfb3e360d6d5e6e800ee4919320cdac02c1cdb
Josef Vonasek <j.f.vonasek@gmail.com>
2020-03-23 12:03:42 +0100

    add comments

--- build/run.js
+++ build/run.js
@@ -183,6 +183,7 @@ commands.test.rust = async function(argv) {
 
 commands.lint = command(`Lint the codebase`)
 commands.lint.rust = async function() {
+    // We run clippy-preview due to https://github.com/rust-lang/rust-clippy/issues/4612
     await run('cargo',['clippy-preview','-Z','unstable-options','--','-D','warnings'])
 }
 
