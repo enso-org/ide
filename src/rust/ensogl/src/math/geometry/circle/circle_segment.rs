1d1ca4af729fe895625765aacf3ede44f7681f3c
Michael Mauderer <michael@michaelmauderer.com>
2020-04-02 13:47:52 +0200

    Generalise math helper functions.

--- src/rust/ensogl/src/math/geometry/circle/circle_segment.rs
+++ src/rust/ensogl/src/math/geometry/circle/circle_segment.rs
@@ -1,6 +1,6 @@
 //! Provides functionality related to circle segments.
-use crate::math::topology::unit::{Angle, Radians};
-
+use std::ops::*;
+use crate::math::{Cos, Sin};
 
 
 // =====================
@@ -10,46 +10,47 @@ use crate::math::topology::unit::{Angle, Radians};
 /// Implements computations related to circle segments.
 /// For details and background on the formulas used, see
 /// https://en.wikipedia.org/wiki/Circular_segment
-#[derive(Clone,Copy,Debug,PartialEq)]
-pub struct CircleSegment{
-    radius: f32,
-    angle : Angle<Radians>,
+#[derive(Clone,Debug,PartialEq)]
+pub struct CircleSegment<S>{
+    radius: S,
+    angle : S,
 }
 
-impl CircleSegment{
+impl<S> CircleSegment<S>
+where S: Add<S,Output=S> + Mul<S,Output=S> + Sub<S,Output=S> + Sin + Cos + From<f32> + Clone{
 
     /// Constructor.
-    pub fn new(radius:f32, angle:Angle<Radians>) -> Self{
+    pub fn new(radius:S, angle:S) -> Self{
         CircleSegment{radius, angle}
     }
 
     /// The length of the direct line between the segment end points.
-    pub fn chord_length(self) -> f32{
-        let r     = self.radius;
-        let theta = self.angle.value;
+    pub fn chord_length(&self) -> S{
+        let radius = self.radius.clone();
+        let theta  = self.angle.clone();
 
-        2.0 * r * (theta * 0.5).sin()
+       S::from(2.0_f32) * radius * (theta * S::from(0.5_f32)).sin()
     }
 
     /// The arc length of the circle segment.
-    pub fn arc_length(self) -> f32{
-        let r     = self.radius;
-        let theta = self.angle.value;
+    pub fn arc_length(&self) -> S{
+        let radius = self.radius.clone();
+        let theta  = self.angle.clone();
 
-        r * theta
+        radius * theta
     }
 
     /// The sagitta (height) of the segment.
-    pub fn sagitta(self) -> f32{
-        let r     = self.radius;
-        let theta = self.angle.value;
+    pub fn sagitta(&self) -> S{
+        let radius = self.radius.clone();
+        let theta  = self.angle.clone();
 
-        r * (1.0 - (theta * 0.5).cos())
+        radius * (S::from(1.0_f32) - (theta * S::from(0.5_f32)).cos())
     }
 
     /// The apothem (height) of the triangular portion.
-    pub fn apothem(self) -> f32{
-        self.radius - self.sagitta()
+    pub fn apothem(self) -> S{
+        self.radius.clone() - self.sagitta()
     }
 
 }
--- src/rust/ensogl/src/math/geometry/triangle.rs
+++ src/rust/ensogl/src/math/geometry/triangle.rs
@@ -1,26 +1,32 @@
-use crate::math::topology::unit::{Radians, Angle};
 use core::f32::consts::PI;
+use crate::display::shape::Var;
+use crate::math::algebra::{Sin, Cos, Sqrt};
+use crate::math::topology::unit::{Radians, Angle};
+use std::ops::*;
 
-pub struct Triangle {
-    pub a     : f32,
-    pub b     : f32,
-    pub c     : f32,
-    pub alpha : Angle<Radians>,
-    pub beta  : Angle<Radians>,
-    pub gamma : Angle<Radians>,
+pub struct Triangle<S> {
+    pub a     : S,
+    pub b     : S,
+    pub c     : S,
+    pub alpha : S,
+    pub beta  : S,
+    pub gamma : S,
 }
 
-impl Triangle{
+impl<S> Triangle<S>
+    where S: Sub<S,Output=S> + Add<S,Output=S> + Mul<S,Output=S> +Div<S,Output=S> + Sin + Cos + Clone + Sqrt + From<f32> {
     /// Two sides and the included angle given (SAS)
-    pub fn from_sas(a:f32, b:f32, gamma:Angle<Radians>) -> Triangle {
-        let c_squared = a.powi(2) + b.powi(2) - (2.0 * a * b * gamma.value.cos());
-        let c = c_squared.sqrt();
+    /// TODO[mm]: testing
+    pub fn from_sas(a:S, b:S, gamma:S) -> Triangle<S> {
+        let a2 = (a.clone() * a.clone());
+        let b2 = (b.clone() * b.clone());
 
-        let alpha = (b.powi(2) + c.powi(2) - a.powi(2)) / (2.0 * b * c);
-        let alpha = Angle::new(alpha);
+        let c2 = a2.clone() + b2.clone() - (S::from(2.0_f32) * a.clone() * b.clone() * gamma.cos());
+        let c = c2.sqrt();
 
-        let beta = PI - alpha.value - gamma.value;
-        let beta = Angle::new(beta);
+        let alpha = (b2.clone() + c2.clone() - a2.clone()) / (S::from(2.0_f32) * b.clone() * c.clone());
+
+        let beta = S::from(PI) - alpha.clone() - gamma.clone();
 
         Triangle{a,b,c,alpha,beta,gamma}
     }
