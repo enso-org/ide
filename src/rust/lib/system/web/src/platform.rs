8c9fe09d18730cd2ae5d7ec0b36aa91272e4f031
Danilo Guanabara <danguafer@gmail.com>
2020-04-07 15:39:11 -0300

    Lints

--- src/rust/lib/system/web/src/platform.rs
+++ src/rust/lib/system/web/src/platform.rs
@@ -18,22 +18,22 @@ impl Platform {
     pub fn query() -> Self {
         let window = window();
         let navigator = window.navigator();
-        let platform  = navigator.platform().unwrap_or("Unknown".into());
-        let agent     = navigator.user_agent().unwrap_or("Unknown".into());
+        let platform  = navigator.platform().unwrap_or_else(|_| "Unknown".into());
+        let agent     = navigator.user_agent().unwrap_or_else(|_| "Unknown".into());
 
         const MACOS_PLATFORMS   : [&str; 4] = ["Macintosh", "MacIntel", "MacPPC", "Mac68K"];
         const WINDOWS_PLATFORMS : [&str; 4] = ["Win32", "Win64", "Windows", "WinCE"];
         const IOS_PLATFORMS     : [&str; 3] = ["iPhone", "iPad", "iPod"];
 
-        if let Some(_) = MACOS_PLATFORMS.iter().find(|item| **item == platform) {
+        if MACOS_PLATFORMS.iter().any(|item| **item == platform) {
             Platform::MacOS
-        } else if let Some(_) = IOS_PLATFORMS.iter().find(|item| **item == platform) {
+        } else if IOS_PLATFORMS.iter().any(|item| **item == platform) {
             Platform::IOS
-        } else if let Some(_) = WINDOWS_PLATFORMS.iter().find(|item| **item == platform) {
+        } else if WINDOWS_PLATFORMS.iter().any(|item| **item == platform) {
             Platform::Windows
-        } else if let Some(_) = agent.find("Android") {
+        } else if agent.find("Android").is_some() {
             Platform::Android
-        } else if let Some(_) = platform.find("Linux") {
+        } else if platform.find("Linux").is_some() {
             Platform::Linux
         } else {
             Platform::Unknown
