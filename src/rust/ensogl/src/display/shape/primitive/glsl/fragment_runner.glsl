b9149edf02c35a0b47daf4e20c7f786265fb3392
Michael Mauderer <michael@michaelmauderer.com>
2020-04-08 15:16:14 +0200

    Add display mode that can visualise shape IDs.

--- src/rust/ensogl/src/display/shape/primitive/glsl/fragment_runner.glsl
+++ src/rust/ensogl/src/display/shape/primitive/glsl/fragment_runner.glsl
@@ -4,6 +4,12 @@ Env   env      = Env(1);
 vec2  position = input_local.xy ;
 Shape shape    = run(env,position);
 float alpha  = shape.color.color.raw.a;
+uint alpha_no_aa = alpha > 0.5 ? uint(1) : uint(0);
+
+output_id = uvec4(input_symbol_id,input_instance_id,0,alpha_no_aa);
+output_id.r *= alpha_no_aa;
+output_id.g *= alpha_no_aa;
+output_id.b *= alpha_no_aa;
 
 if (input_display_mode == 0) {
     output_color = srgba(unpremultiply(shape.color)).raw;
@@ -11,11 +17,8 @@ if (input_display_mode == 0) {
 } else if (input_display_mode == 1) {
     Rgb col = distance_meter(shape.sdf.distance, 200.0 * input_zoom * input_pixel_ratio, 200.0/input_zoom * input_pixel_ratio);
     output_color = rgba(col).raw;
+}else if (input_display_mode == 2) {
+    float r = float(((int(output_id.r) * 79) % 360) / 360);
+    float g = float(((int(output_id.r) * 43) % 360) / 360);
+    output_color = vec4(r, g, 0.0, float(alpha_no_aa));
 }
-
-uint alpha_no_aa = alpha > 0.5 ? uint(1) : uint(0);
-
-output_id = uvec4(input_symbol_id,input_instance_id,0,alpha_no_aa);
-output_id.r *= alpha_no_aa;
-output_id.g *= alpha_no_aa;
-output_id.b *= alpha_no_aa;
