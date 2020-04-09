a8cd62e5d75b23edb444cc421b2bb17b0097472f
Danilo Guanabara <danguafer@gmail.com>
2020-04-09 18:42:47 -0300

    Making use of hashmaps

--- src/rust/lib/prelude/src/lib.rs
+++ src/rust/lib/prelude/src/lib.rs
@@ -38,13 +38,16 @@ pub use ifmt::*;
 pub use itertools::Itertools;
 pub use lazy_static::lazy_static;
 pub use num::Num;
+pub use maplit::btreemap;
+pub use maplit::btreeset;
+pub use maplit::convert_args;
+pub use maplit::hashmap;
+pub use maplit::hashset;
 pub use paste;
 pub use shrinkwraprs::Shrinkwrap;
 pub use weak_table::traits::WeakElement;
 pub use weak_table::WeakValueHashMap;
 pub use weak_table;
-
-
 
 // ================
 // === CloneRef ===
--- src/rust/lib/system/web/src/platform.rs
+++ src/rust/lib/system/web/src/platform.rs
@@ -2,8 +2,10 @@
 
 use super::window;
 
+use enso_prelude::hashmap;
+
 /// This enumeration lists all the supported platforms.
-#[derive(Debug,Clone,Copy)]
+#[derive(Debug,Clone,Copy,PartialEq)]
 pub enum Platform {
     Linux,
     Android,
@@ -21,16 +23,22 @@ impl Platform {
         let platform  = navigator.platform().unwrap_or_else(|_| "Unknown".into());
         let agent     = navigator.user_agent().unwrap_or_else(|_| "Unknown".into());
 
-        const MACOS_PLATFORMS   : [&str; 4] = ["Macintosh", "MacIntel", "MacPPC", "Mac68K"];
-        const WINDOWS_PLATFORMS : [&str; 4] = ["Win32", "Win64", "Windows", "WinCE"];
-        const IOS_PLATFORMS     : [&str; 3] = ["iPhone", "iPad", "iPod"];
+        let platforms = hashmap!{
+            String::from("Macintosh") => Platform::MacOS,
+            String::from("MacIntel")  => Platform::MacOS,
+            String::from("MacPPC")    => Platform::MacOS,
+            String::from("Mac68K")    => Platform::MacOS,
+            String::from("Win32")     => Platform::Windows,
+            String::from("Win64")     => Platform::Windows,
+            String::from("Windows")   => Platform::Windows,
+            String::from("WinCE")     => Platform::Windows,
+            String::from("iPhone")    => Platform::IOS,
+            String::from("iPad")      => Platform::IOS,
+            String::from("iPod")      => Platform::IOS
+        };
 
-        if MACOS_PLATFORMS.iter().any(|item| **item == platform) {
-            Platform::MacOS
-        } else if IOS_PLATFORMS.iter().any(|item| **item == platform) {
-            Platform::IOS
-        } else if WINDOWS_PLATFORMS.iter().any(|item| **item == platform) {
-            Platform::Windows
+        if let Some(platform) = platforms.get(&platform) {
+            *platform
         } else if agent.find("Android").is_some() {
             Platform::Android
         } else if platform.find("Linux").is_some() {
@@ -38,5 +46,35 @@ impl Platform {
         } else {
             Platform::Unknown
         }
+    }
+}
+
+#[cfg(all(test,any(host_os="linux",host_os="windows",host_os="macos")))]
+mod test {
+    use super::Platform;
+
+    use wasm_bindgen_test::wasm_bindgen_test;
+    use wasm_bindgen_test::wasm_bindgen_test_configure;
+
+    wasm_bindgen_test_configure!(run_in_browser);
+
+    #[cfg(host_os = "linux")]
+    fn host_platform() -> Platform {
+        Platform::Linux
+    }
+
+    #[cfg(host_os = "windows")]
+    fn host_platform() -> Platform {
+        Platform::Windows
+    }
+
+    #[cfg(host_os = "macos")]
+    fn host_platform() -> Platform {
+        Platform::MacOS
+    }
+
+    #[wasm_bindgen_test]
+    fn platform() {
+        assert_eq!(Platform::query(), host_platform())
     }
 }
