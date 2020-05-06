098dbc2012075a535c6ef0f219bd574fbe624077
Josef Vonasek <j.f.vonasek@gmail.com>
2020-05-06 17:14:01 +0200

    Include generated files.

--- src/rust/ide/engine-api/build.rs
+++ src/rust/ide/engine-api/build.rs
@@ -1,11 +1,9 @@
-use ensogl_build_utilities::absolute_path;
-
+use flatc_rust;
+use std::env;
 use std::fs;
 use std::fs::File;
-use std::fs::create_dir_all;
 use std::io::prelude::*;
 use std::path::PathBuf;
-use std::process::Command;
 use zip_extensions::read::ZipArchiveExtensions;
 
 
@@ -14,32 +12,27 @@ use zip_extensions::read::ZipArchiveExtensions;
 // == Hardcoded constants ==
 // =========================
 
+/// The name of zip containing engine interface files.
+const ZIP_NAME:&str = "fbs-schema.zip";
 
-/// The path where downloaded artefacts will be stored.
-const ZIP_PATH   : &str = "./pkg/fbs-schema.zip";
-/// The path where downloaded artefacts will be unziped.
-const UNZIP_PATH : &str = "./pkg/fbs-upload/fbs-schema/";
+/// The directory structure inside downloaded engine interface folder.
+const ZIP_CONTENT:&str = "fbs-upload/fbs-schema/";
 
 /// Commit from `enso` repository that will be used to obtain artefacts from.
-const COMMIT: &str = "25494bbb4315d7d6d625424280dfdee0e49dd045";
+/// If you change this commit manually, you must have `flatc` installed to regenrate inteface files.
+/// If you are contributor, you are obligated to also run `cargo build` before creating a commit
+/// with such change!
+/// Hint: You can install flatc with `conda -c conda-forge install flatbuffers=1.12.0`
+const COMMIT:&str = "29190f83392e5da04172c36ea432c5410770da0f";
 
-/// Obtains a URL where the artefacts can be downloaded.
-pub fn flatbuffers_url() -> reqwest::Url {
-    let url = format!("https://packages.luna-lang.org/fbs-schema/nightly/25494bbb4315d7d6d625424280dfdee0e49dd045/fbs-schema.zip");
+/// An URL pointing to engine interface files.
+pub fn interface_description_url() -> reqwest::Url {
+    let url = format!("https://packages.luna-lang.org/fbs-schema/nightly/{}/fbs-schema.zip",COMMIT);
     let err = format!("{} is an invalid URL.",url);
     reqwest::Url::parse(&url).expect(&err)
 }
 
-pub fn flatc(file:PathBuf) {
-    let file_str = file.to_str().expect("Invalid UTF8 file path");
-    let result   = Command::new("flatc")
-        .args(&["--rust", "-o", "./src/generated", file_str])
-        .output()
-        .expect("Command `flatc` failed to execute");
-    if !result.status.success() {
-        panic!("Command `flatc` returned error exit code.")
-    }
-}
+
 
 // ===================================
 // == Download Engine Api Artefacts ==
@@ -48,22 +41,19 @@ pub fn flatc(file:PathBuf) {
 /// Struct for downloading engine artefacts.
 struct ApiProvider {
     /// The path where downloaded artefacts will be stored.
-    zip_path: PathBuf,
-    /// The path where downloaded artefacts will be unziped.
-    unzip_path: PathBuf,
+    out_dir: PathBuf,
 }
 
 impl ApiProvider {
     /// Creates a provider that can download engine artefacts.
     pub fn new() -> ApiProvider {
-        let zip_path   = PathBuf::from(absolute_path(ZIP_PATH).expect("Invalid path"));
-        let unzip_path = PathBuf::from(absolute_path(UNZIP_PATH).expect("Invalid path"));
-        ApiProvider{zip_path,unzip_path}
+        let out_dir_str = env::var("OUT_DIR").expect("OUT_DIR isn't environment variable");
+        ApiProvider{out_dir:out_dir_str.into()}
     }
 
     /// Downloads api artefacts into memory.
     pub async fn download(&self) -> bytes::Bytes {
-        let url            = flatbuffers_url();
+        let url            = interface_description_url();
         let get_error      = format!("Failed to get response from {}",    &url);
         let download_error = format!("Failed to download contents of {}", &url);
         let response       = reqwest::get(url).await.expect(&get_error);
@@ -72,57 +62,57 @@ impl ApiProvider {
 
     /// Saves unzipped artefacts into file.
     pub fn unzip(&self, artefacts:bytes::Bytes) {
-        let display_path = self.zip_path.display();
+        let zip_path     = self.out_dir.join(ZIP_NAME);
+        let display_path = zip_path.display();
         let open_error   = format!("Failed to open {}", display_path);
         let write_error  = format!("Failed to write {}",display_path);
         let flush_error  = format!("Failed to flush {}",display_path);
         let unzip_error  = format!("Failed to unzip {}",display_path);
 
-        let mut file = File::create(&self.zip_path).expect(&open_error);
+        let mut file = File::create(&zip_path).expect(&open_error);
         file.write_all(&artefacts).expect(&write_error);
         file.flush().expect(&flush_error);
 
-        let file = File::open(&self.zip_path).expect(&open_error);
-        let root = self.zip_path.parent().expect("Unable to access parent directory");
-        let mut archive = zip::ZipArchive::new(file).expect(&open_error);
-        archive.extract(&root.to_path_buf()).expect(&unzip_error);
+        let file        = File::open(&zip_path).expect(&open_error);
+        let mut archive = zip::ZipArchive::new(&file).expect(&open_error);
+        archive.extract(&self.out_dir).expect(&unzip_error);
     }
 
+
+    /// Generates rust files from flatbuffers schemas.
     pub fn generate_files(&self) {
-        for entry in fs::read_dir(&self.unzip_path).expect("Could not read content of dir") {
-            let entry = entry.expect("Invalid content of dir");
-            flatc(entry.path());
+        let fbs_dir = self.out_dir.join(ZIP_CONTENT);
+        for entry in fs::read_dir(&fbs_dir).expect("Could not read content of dir") {
+            let path = entry.expect("Invalid content of dir").path();
+            let result = flatc_rust::run(flatc_rust::Args {
+                inputs  : &[&path],
+                out_dir : &PathBuf::from("./src"),
+                ..Default::default()
+            });
+            if result.is_err() {
+                println!("cargo:info=Engine API files were not regenerated because `flatc` isn't installed");
+                break;
+            }
         }
-    }
-
-    /// Ensures that target's parent directory exists.
-    pub fn prepare_target_location(&self) {
-        let parent_directory = self.zip_path.parent().expect("Unable to access parent directory");
-        let create_dir_error = format!(
-            "Failed to create directory: {}.",
-            parent_directory.display());
-        create_dir_all(parent_directory).expect(&create_dir_error);
     }
 
     /// Places required artefacts in the target location.
     pub async fn run(&self) {
-        self.prepare_target_location();
-        let parent_directory = self.zip_path.parent().expect("Unable to access parent directory.");
-        let fingerprint      = parent_directory.join("artefacts.fingerprint");
-        let opt_version      = fs::read_to_string(&fingerprint);
-        let changed          = match opt_version {
-            Err(_)   => true,
-            Ok(hash) => hash != COMMIT
+        let fingerprint = self.out_dir.join("egine.api.fingerprint");
+        let unchanged   = match fs::read_to_string(&fingerprint) {
+            Ok(commit) => commit == COMMIT,
+            Err(_)     => false,
         };
-        if !changed {return}
+        if unchanged {return}
 
-        println!("cargo:warning=Engine API artefacts version changed. Rebuilding.");
+        println!("cargo:info=Engine API artefacts version changed. Rebuilding.");
         let artefacts = self.download().await;
         self.unzip(artefacts);
         self.generate_files();
         fs::write(&fingerprint,COMMIT).expect("Unable to write artefacts fingerprint.");
     }
 }
+
 
 
 // ==========
---
+++ src/rust/ide/engine-api/src/lib.rs
@@ -0,0 +1,10 @@
+#[allow(dead_code, unused_imports)]
+
+//! Crate containing the engine interface files.
+
+use flatbuffers;
+
+pub mod envelope_generated;
+pub mod execution_context_generated;
+pub mod session_generated;
+pub mod util_generated;
