4bc96eab53dd0603de19167ef2bc00bd92b47a31
Michael Mauderer <michael.mauderer@enso.org>
2020-05-11 10:32:46 +0200

    Refactor sample data code.

--- src/rust/lib/graph-editor/src/component/visualization/data.rs
+++ src/rust/lib/graph-editor/src/component/visualization/data.rs
@@ -69,3 +69,34 @@ pub enum DataError {
     InternalComputationError,
 }
 
+
+// =============================
+// === Sample Data Generator ===
+// =============================
+// TODO this will go away once we have real data
+
+#[derive(Clone,CloneRef,Debug,Default)]
+pub(crate) struct SampleDataGenerator3D {
+    counter: Rc<Cell<f32>>
+}
+
+impl SampleDataGenerator3D {
+
+    pub fn generate_data(&self) -> Vec<Vector3<f32>> {
+
+        let current_value = self.counter.get();
+        self.counter.set(current_value + 0.1);
+
+        let delta1 = current_value.sin() * 10.0;
+        let delta2 = current_value.cos() * 10.0;
+
+       vec![
+            Vector3::new(25.0,75.0,25.0 + delta1),
+            Vector3::new(25.0,25.0, 25.0 + delta2),
+            Vector3::new(75.0 - 12.5,75.0 + delta1,12.5),
+            Vector3::new(75.0 + 12.5,75.0 + delta2,12.5),
+            Vector3::new(75.0 - 12.5 + delta1,25.0 + delta2,12.5),
+            Vector3::new(75.0 + 12.5 + delta2,25.0 + delta1,12.5),
+        ]
+    }
+}
