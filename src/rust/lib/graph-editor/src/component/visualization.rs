965406815740bcebe8bcc79ba5b6d8042b99f7a2
Michael Mauderer <michael.mauderer@enso.org>
2020-05-13 17:38:36 +0200

    Further clarify docs.

--- src/rust/lib/graph-editor/src/component/visualization.rs
+++ src/rust/lib/graph-editor/src/component/visualization.rs
@@ -2,20 +2,17 @@
 //!
 //! The overall architecture of visualizations consists of three parts:
 //!
-//! 1. the `DataRenderer` is a trait that sits at the core of the visualisation system. A
-//!    `DataRenderer` provides the actual visualization view that implements `display::Object`.
-//!    It is provided with data and itself provides frp streams of its output (if there is some,
-//!    e.g., if it acts as a widget).
+//! 1. the `DataRenderer` trait provides the functionality to render the actual visualization view
+//!     that implements `display::Object`. It is provided with data and itself provides frp streams
+//!     of its output, if there is some, e.g., if it acts as a widget.
 //!
 //! 2. the `Visualization` is a struct that wraps the `DataRenderer` and implements the generic
 //!    tasks that are the same for all visualisations. That is, interfacing with the other UI
-//!    elements, providing data updates to the `DataRenderer`, and propagating information about
-//!    the state changes in the `DataRenderer`.
+//!    elements, the visualization registry, as well as propagating frp messages.
 //!
 //! 3. the `Container` wraps the `Visualisation` and provides the UI elements that facilitate
-//!    generic interactions, for example, selecting a specific visualisation or setting input data
-//!    for a `Visualisation`. The `Container` also provides the FRP API that allows internal
-//!    interaction with the `Visualisation`.
+//!    user interactions. For example, selecting a visualisation or connecting it to nodes in the
+//!    graph editor scene.
 //!
 //! In addition this module also contains a `Data` struct that provides a dynamically typed way to
 //! handle data for visualisations. This allows the `Visualisation` struct to be without type
