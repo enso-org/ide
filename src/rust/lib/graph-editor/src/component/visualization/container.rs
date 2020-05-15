b8c77365217eb69af3b0d78abf7873fa820a40f4
Michael Mauderer <michael.mauderer@enso.org>
2020-05-15 16:10:35 +0200

    Implement basic visual representation of visualization UI element.

--- src/rust/lib/graph-editor/src/component/visualization/container.rs
+++ src/rust/lib/graph-editor/src/component/visualization/container.rs
@@ -5,9 +5,61 @@ use crate::prelude::*;
 use crate::frp;
 use crate::visualization::*;
 
-use ensogl::display::Scene;
+use ensogl::data::color;
+use ensogl::display::Attribute;
+use ensogl::display::Buffer;
+use ensogl::display::Sprite;
+use ensogl::display::scene::Scene;
+use ensogl::display::shape::*;
 use ensogl::display::traits::*;
 use ensogl::display;
+use ensogl::gui::component;
+use ensogl::math::topology::unit::PixelDistance;
+use ensogl::display::layout::alignment;
+use ensogl::gui::component::animation;
+
+
+// =============
+// === Shape ===
+// =============
+
+/// Canvas node shape definition.
+pub mod frame {
+    use super::*;
+
+    ensogl::define_shape_system! {
+        (width:f32,height:f32,selected:f32,padding:f32) {
+            // TODO use style
+
+            let width_bg       = width.clone();
+            let height_bg      = height.clone();
+            let width_bg  : Var<Distance<Pixels>> = width_bg.into();
+            let height_bg : Var<Distance<Pixels>> = height_bg.into();
+            let radius    : Var<Distance<Pixels>> = padding.clone().into();
+            let color_bg    = color::Lcha::new(0.2,0.013,0.18,1.0);
+            let background  = Rect((&width_bg,&height_bg)).corners_radius(&radius);
+            let background  = background.fill(color::Rgba::from(color_bg));
+
+            let frame_outer = Rect((&width_bg,&height_bg)).corners_radius(&radius);
+
+            // +1 at the end to avoid aliasing artifacts.
+            let width_frame_inner       =  &width  - &padding * Var::<f32>::from(2.0) * &selected + Var::<f32>::from(1.0);
+            let height_frame_inner      =  &height - &padding * Var::<f32>::from(2.0) * &selected + Var::<f32>::from(1.0);
+            let width_frame_inner  : Var<Distance<Pixels>> = width_frame_inner.into();
+            let height_frame_inner : Var<Distance<Pixels>> = height_frame_inner.into();
+            let inner_radius = &radius * (Var::<f32>::from(1.0) - &selected);
+            let frame_inner = Rect((&width_frame_inner,&height_frame_inner)).corners_radius(&inner_radius);
+
+            let frame = frame_outer.difference(frame_inner);
+            let color_frame    = color::Lcha::new(0.72,0.5,0.22,1.0);
+            let frame = frame.fill(color::Rgba::from(color_frame));
+
+             let out = background + frame;
+
+             out.into()
+        }
+    }
+}
 
 
 // ===========
@@ -23,6 +75,8 @@ pub struct ContainerFrp {
     pub toggle_visibility : frp::Source,
     pub set_visualization : frp::Source<Option<Visualization>>,
     pub set_data          : frp::Source<Option<Data>>,
+    pub select            : frp::Source,
+    pub deselect          : frp::Source,
 }
 
 impl Default for ContainerFrp {
@@ -32,9 +86,11 @@ impl Default for ContainerFrp {
             def toggle_visibility = source();
             def set_visualization = source();
             def set_data          = source();
+            def select            = source();
+            def deselect          = source();
         };
         let network = visualization_events;
-        Self {network,set_visibility,set_visualization,toggle_visibility,set_data }
+        Self {network,set_visibility,set_visualization,toggle_visibility,set_data,select,deselect }
     }
 }
 
@@ -65,31 +121,29 @@ pub struct Container {
 #[derive(Debug,Clone)]
 #[allow(missing_docs)]
 pub struct ContainerData {
-    logger         : Logger,
-    display_object : display::object::Instance,
-    size           : Cell<Vector2<f32>>,
-    visualization  : RefCell<Option<Visualization>>,
+    logger                  : Logger,
+    size                    : Cell<Vector2<f32>>,
+    padding                 : Cell<f32>,
+    display_object          : display::object::Instance,
+    display_object_internal : display::object::Instance,
+    visualization           : RefCell<Option<Visualization>>,
+    frame                   : component::ShapeView<frame::Shape>,
+
 }
 
 impl ContainerData {
     /// Set whether the visualization should be visible or not.
     pub fn set_visibility(&self, is_visible:bool) {
-        if let Some(vis) = self.visualization.borrow().as_ref() {
-            if is_visible {
-                self.add_child(vis);
-            } else {
-                vis.unset_parent();
-            }
+        if is_visible {
+            self.display_object_internal.set_parent(&self.display_object);
+        } else {
+            self.display_object_internal.unset_parent();
         }
     }
 
     /// Indicates whether the visualization is visible.
     pub fn is_visible(&self) -> bool {
-        if let Some(vis) = self.visualization.borrow().as_ref() {
-            vis.has_parent()
-        } else {
-            false
-        }
+        self.display_object_internal.has_parent()
     }
 
     /// Toggle visibility.
@@ -108,9 +162,9 @@ impl ContainerData {
         self.set_visibility(true);
     }
 
-    /// Set the visualization shown in this container..
-    fn set_visualization(&self, visualization:Visualization) {
-        self.add_child(&visualization);
+    /// Set the visualization shown in this container.
+    fn set_visualisation(&self, visualization:Visualization) {
+        visualization.display_object().set_parent(&self.display_object_internal);
         self.visualization.replace(Some(visualization));
         self.init_visualization_properties();
     }
@@ -126,27 +180,71 @@ impl display::Object for ContainerData {
 impl Container {
     /// Constructor.
     pub fn new(scene:&Scene) -> Self {
-        let logger         = Logger::new("visualization");
-        let visualization  = default();
-        let size           = Cell::new(Vector2::new(200.0, 200.0));
-        let display_object = display::object::Instance::new(&logger);
-        let data           = ContainerData {logger,visualization,size,display_object};
-        let data           = Rc::new(data);
+        let logger                  = Logger::new("visualization");
+        let visualization           = default();
+        let size                    = Cell::new(Vector2::new(200.0, 200.0));
+        let display_object          = display::object::Instance::new(&logger);
+        let display_object_internal = display::object::Instance::new(&logger);
+        let padding                 = Cell::new(10.0);
+        let frame                   = component::ShapeView::<frame::Shape>::new(&logger,scene);
+        let data                    = ContainerData {logger,visualization,size,display_object,frame,
+                                                     display_object_internal,padding};
+        let data                    = Rc::new(data);
         data.set_visualization(Registry::default_visualisation(&scene));
         data.set_visibility(false);
-        let frp            = default();
-        Self {data,frp} . init_frp()
+        let frp                     = default();
+        Self {data,frp} . init(scene) . init_frp()
+    }
+
+    fn init(self, scene:&Scene) ->  Self {
+        self.init_frame(scene)
+    }
+
+    fn init_frame(self, scene:&Scene) -> Self {
+        let shape_system = scene.shapes.shape_system(PhantomData::<frame::Shape>);
+        shape_system.shape_system.set_alignment(
+            alignment::HorizontalAlignment::Center,
+            alignment::VerticalAlignment::Center
+        );
+
+        let frame_shape = &self.data.frame.shape;
+        let padding     = self.data.padding.get();
+        let width       = self.data.size.get().x;
+        let height      = self.data.size.get().y;
+        frame_shape.width.set(width + 2.0 * padding);
+        frame_shape.height.set(height + 2.0 * padding);
+        frame_shape.padding.set(padding);
+        frame_shape.sprite.size().set(Vector2::new(width + 2.0 * padding, height + 2.0 * padding));
+        frame_shape.selected.set(0.0);
+
+        frame_shape.mod_position(|t| t.x += width/2.0);
+        frame_shape.mod_position(|t| t.y += height/2.0);
+        self.display_object_internal.add_child(&self.data.frame);
+        self
+
+    }
+
+    fn set_selected(&self, value:bool) {
+        if value {
+            self.data.frame.shape.selected.set(1.0);
+        } else{
+            self.data.frame.shape.selected.set(0.0);
+        }
     }
 
     fn init_frp(self) -> Self {
         let frp     = &self.frp;
         let network = &self.frp.network;
+        let container_data = &self.data;
+
+        let frame_shape_data = container_data.frame.shape.clone_ref();
+        let selection = animation(network, move |value| {
+            frame_shape_data.selected.set(value)
+        });
 
         frp::extend! { network
 
-            let container_data = &self.data;
-
-            def _f_hide = frp.set_visibility.map(f!([container_data](is_visible) {
+            def _f_hide = frp.set_visibility.map(f!((container_data)(is_visible) {
                 container_data.set_visibility(*is_visible);
             }));
 
@@ -163,6 +261,22 @@ impl Container {
             def _f_set_data = frp.set_data.map(f!([container_data](data) {
                  container_data.visualization.borrow()
                     .for_each_ref(|vis| vis.frp.set_data.emit(data));
+            }));
+
+            def _select = frp.select.map(f!((selection)(_) {
+                 selection.set_target_position(1.0);
+            }));
+
+            def _deselect = frp.select.map(f!((selection)(_) {
+                 selection.set_target_position(1.0);
+            }));
+
+            def _output_show = container_data.frame.events.mouse_over.map(f!((selection)(_) {
+                selection.set_target_position(1.0);
+            }));
+
+            def _output_hide = container_data.frame.events.mouse_out.map(f!((selection)(_) {
+                selection.set_target_position(0.0);
             }));
         }
         self
