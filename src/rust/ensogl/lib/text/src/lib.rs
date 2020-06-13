fd101ad58a762160adc5ceb7d1b0b0d88f0242ef
Wojciech Danilo <wojciech.danilo@gmail.com>
2020-06-14 01:21:23 +0200

    Refactoring

--- src/rust/ensogl/lib/text/src/lib.rs
+++ src/rust/ensogl/lib/text/src/lib.rs
@@ -172,13 +172,17 @@ pub struct View {
 
 
 impl LineOffset for View {
-    fn offset_of_line(&self, text:&Rope, line:usize) -> usize {
-        let line = line.min(text.measure::<LinesMetric>() + 1);
-        text.offset_of_line(line)
+    fn text(&self) -> &Rope {
+        &self.buffer.rope
     }
 
-    fn line_of_offset(&self, text:&Rope, offset:usize) -> usize {
-        text.line_of_offset(offset)
+    fn offset_of_line(&self,line:usize) -> usize {
+        let line = line.min(self.text().measure::<LinesMetric>() + 1);
+        self.text().offset_of_line(line)
+    }
+
+    fn line_of_offset(&self,offset:usize) -> usize {
+        self.text().line_of_offset(offset)
     }
 }
 
@@ -255,105 +259,102 @@ impl View {
 }
 
 
-/// Based on the current selection position this will return the cursor position, the current line, and the
+impl View {
+    /// Based on the current selection position this will return the cursor position, the current line, and the
 /// total number of lines of the file.
-fn selection_position(
-    r: SelRegion,
-    lo: &dyn LineOffset,
-    text: &Rope,
-    move_up: bool,
-    modify: bool,
-) -> (HorizPos, usize) {
-    // The active point of the selection
-    let active = if modify {
-        r.end
-    } else if move_up {
-        r.min()
-    } else {
-        r.max()
-    };
-    let col = if let Some(col) = r.horiz { col } else { lo.offset_to_line_col(text, active).1 };
-    let line = lo.line_of_offset(text, active);
+    fn selection_position(&self,
+        r: SelRegion,
+        text: &Rope,
+        move_up: bool,
+        modify: bool,
+    ) -> (HorizPos, usize) {
+        // The active point of the selection
+        let active = if modify {
+            r.end
+        } else if move_up {
+            r.min()
+        } else {
+            r.max()
+        };
+        let col = if let Some(col) = r.horiz { col } else { self.offset_to_line_col(active).1 };
+        let line = self.line_of_offset(active);
 
-    (col, line)
-}
+        (col, line)
+    }
 
 
-/// Compute movement based on vertical motion by the given number of lines.
+    /// Compute movement based on vertical motion by the given number of lines.
 ///
 /// Note: in non-exceptional cases, this function preserves the `horiz`
 /// field of the selection region.
-fn vertical_motion(
-    r: SelRegion,
-    lo: &dyn LineOffset,
-    text: &Rope,
-    line_delta: isize,
-    modify: bool,
-) -> (usize, Option<HorizPos>) {
-    let (col, line) = selection_position(r, lo, text, line_delta < 0, modify);
-    let n_lines = lo.line_of_offset(text, text.len());
+    fn vertical_motion(&self,
+        r: SelRegion,
+        text: &Rope,
+        line_delta: isize,
+        modify: bool,
+    ) -> (usize, Option<HorizPos>) {
+        let (col, line) = self.selection_position(r, text, line_delta < 0, modify);
+        let n_lines = self.line_of_offset(text.len());
 
-    // This code is quite careful to avoid integer overflow.
-    // TODO: write tests to verify
-    if line_delta < 0 && (-line_delta as usize) > line {
-        return (0, Some(col));
+        // This code is quite careful to avoid integer overflow.
+        // TODO: write tests to verify
+        if line_delta < 0 && (-line_delta as usize) > line {
+            return (0, Some(col));
+        }
+        let line = if line_delta < 0 {
+            line - (-line_delta as usize)
+        } else {
+            line.saturating_add(line_delta as usize)
+        };
+        if line > n_lines {
+            return (text.len(), Some(col));
+        }
+        let new_offset = self.line_col_to_offset(line, col);
+        (new_offset, Some(col))
     }
-    let line = if line_delta < 0 {
-        line - (-line_delta as usize)
-    } else {
-        line.saturating_add(line_delta as usize)
-    };
-    if line > n_lines {
-        return (text.len(), Some(col));
-    }
-    let new_offset = lo.line_col_to_offset(text, line, col);
-    (new_offset, Some(col))
-}
 
-/// Compute movement based on vertical motion by the given number of lines skipping
+    /// Compute movement based on vertical motion by the given number of lines skipping
 /// any line that is shorter than the current cursor position.
-fn vertical_motion_exact_pos(
-    r: SelRegion,
-    lo: &dyn LineOffset,
-    text: &Rope,
-    move_up: bool,
-    modify: bool,
-) -> (usize, Option<HorizPos>) {
-    let (col, init_line) = selection_position(r, lo, text, move_up, modify);
-    let n_lines = lo.line_of_offset(text, text.len());
+    fn vertical_motion_exact_pos(&self,
+        r: SelRegion,
+        text: &Rope,
+        move_up: bool,
+        modify: bool,
+    ) -> (usize, Option<HorizPos>) {
+        let (col, init_line) = self.selection_position(r, text, move_up, modify);
+        let n_lines = self.line_of_offset(text.len());
 
-    let mut line_length =
-        lo.offset_of_line(text, init_line.saturating_add(1)) - lo.offset_of_line(text, init_line);
-    if move_up && init_line == 0 {
-        return (lo.line_col_to_offset(text, init_line, col), Some(col));
-    }
-    let mut line = if move_up { init_line - 1 } else { init_line.saturating_add(1) };
+        let mut line_length =
+            self.offset_of_line(init_line.saturating_add(1)) - self.offset_of_line(init_line);
+        if move_up && init_line == 0 {
+            return (self.line_col_to_offset(init_line, col), Some(col));
+        }
+        let mut line = if move_up { init_line - 1 } else { init_line.saturating_add(1) };
 
-    // If the active columns is longer than the current line, use the current line length.
-    let col = if line_length < col { line_length - 1 } else { col };
+        // If the active columns is longer than the current line, use the current line length.
+        let col = if line_length < col { line_length - 1 } else { col };
 
-    loop {
-        line_length = lo.offset_of_line(text, line + 1) - lo.offset_of_line(text, line);
+        loop {
+            line_length = self.offset_of_line(line + 1) - self.offset_of_line(line);
 
-        // If the line is longer than the current cursor position, break.
-        // We use > instead of >= because line_length includes newline.
-        if line_length > col {
-            break;
+            // If the line is longer than the current cursor position, break.
+            // We use > instead of >= because line_length includes newline.
+            if line_length > col {
+                break;
+            }
+
+            // If you are trying to add a selection past the end of the file or before the first line, return original selection
+            if line >= n_lines || (line == 0 && move_up) {
+                line = init_line;
+                break;
+            }
+
+            line = if move_up { line - 1 } else { line.saturating_add(1) };
         }
 
-        // If you are trying to add a selection past the end of the file or before the first line, return original selection
-        if line >= n_lines || (line == 0 && move_up) {
-            line = init_line;
-            break;
-        }
-
-        line = if move_up { line - 1 } else { line.saturating_add(1) };
+        (self.line_col_to_offset(line, col), Some(col))
     }
-
-    (lo.line_col_to_offset(text, line, col), Some(col))
 }
-
-
 
 
 
@@ -405,38 +406,29 @@ impl View {
                 else        { no_horiz(region.max()) }
             }
 
-//        Movement::LeftWord => {
-//            let mut word_cursor = WordCursor::new(text, end);
-//            let offset = word_cursor.prev_boundary().unwrap_or(0);
-//            (offset, None)
-//        }
-//        Movement::RightWord => {
-//            let mut word_cursor = WordCursor::new(text, end);
-//            let offset = word_cursor.next_boundary().unwrap_or_else(|| text.len());
-//            (offset, None)
-//        }
             Movement::LeftOfLine => {
-                let line   = self.line_of_offset(text,end);
-                let offset = self.offset_of_line(text,line);
+                let line   = self.line_of_offset(end);
+                let offset = self.offset_of_line(line);
                 no_horiz(offset)
             }
+            
             Movement::RightOfLine => {
-                let line = self.line_of_offset(text, end);
+                let line = self.line_of_offset(end);
                 let mut offset = text.len();
 
                 // calculate end of line
-                let next_line_offset = self.offset_of_line(text, line + 1);
-                if line < self.line_of_offset(text, offset) {
+                let next_line_offset = self.offset_of_line(line + 1);
+                if line < self.line_of_offset(offset) {
                     if let Some(prev) = text.prev_grapheme_offset(next_line_offset) {
                         offset = prev;
                     }
                 }
                 (offset, None)
             }
-            Movement::Up => vertical_motion(region, self, text, -1, modify),
-            Movement::Down => vertical_motion(region, self, text, 1, modify),
-            Movement::UpExactPosition => vertical_motion_exact_pos(region, self, text, true, modify),
-            Movement::DownExactPosition => vertical_motion_exact_pos(region, self, text, false, modify),
+            Movement::Up => self.vertical_motion(region,text, -1, modify),
+            Movement::Down => self.vertical_motion(region,text, 1, modify),
+            Movement::UpExactPosition => self.vertical_motion_exact_pos(region,text, true, modify),
+            Movement::DownExactPosition => self.vertical_motion_exact_pos(region,text, false, modify),
             Movement::StartOfParagraph => {
                 // Note: TextEdit would start at modify ? end : region.min()
                 let mut cursor = Cursor::new(&text, end);
@@ -477,8 +469,8 @@ impl View {
                 }
                 (offset, None)
             }
-            Movement::UpPage => vertical_motion(region, self, text, -scroll_height(self.scroll_height()), modify),
-            Movement::DownPage => vertical_motion(region, self, text, scroll_height(self.scroll_height()), modify),
+            Movement::UpPage => self.vertical_motion(region,text, -scroll_height(self.scroll_height()), modify),
+            Movement::DownPage => self.vertical_motion(region,text, scroll_height(self.scroll_height()), modify),
             Movement::StartOfDocument => (0, None),
             Movement::EndOfDocument => (text.len(), None),
         };
@@ -698,14 +690,16 @@ pub enum Movement {
 pub trait LineOffset {
     // use own breaks if present, or text if not (no line wrapping)
 
+    fn text(&self) -> &Rope;
+
     /// Returns the byte offset corresponding to the given line.
-    fn offset_of_line(&self, text: &Rope, line: usize) -> usize {
-        text.offset_of_line(line)
+    fn offset_of_line(&self, line:usize) -> usize {
+        self.text().offset_of_line(line)
     }
 
     /// Returns the visible line number containing the given offset.
-    fn line_of_offset(&self, text: &Rope, offset: usize) -> usize {
-        text.line_of_offset(offset)
+    fn line_of_offset(&self, offset:usize) -> usize {
+        self.text().line_of_offset(offset)
     }
 
     // How should we count "column"? Valid choices include:
@@ -718,27 +712,28 @@ pub trait LineOffset {
     // Of course, all these are identical for ASCII. For now we use UTF-8 code units
     // for simplicity.
 
-    fn offset_to_line_col(&self, text: &Rope, offset: usize) -> (usize, usize) {
-        let line = self.line_of_offset(text, offset);
-        (line, offset - self.offset_of_line(text, line))
+    fn offset_to_line_col(&self, offset:usize) -> (usize,usize) {
+        let line = self.line_of_offset(offset);
+        let col  = offset - self.offset_of_line(line);
+        (line,col)
     }
 
-    fn line_col_to_offset(&self, text: &Rope, line: usize, col: usize) -> usize {
-        let mut offset = self.offset_of_line(text, line).saturating_add(col);
-        if offset >= text.len() {
-            offset = text.len();
-            if self.line_of_offset(text, offset) <= line {
+    fn line_col_to_offset(&self, line:usize, col:usize) -> usize {
+        let mut offset = self.offset_of_line(line).saturating_add(col);
+        if offset >= self.text().len() {
+            offset = self.text().len();
+            if self.line_of_offset(offset) <= line {
                 return offset;
             }
         } else {
             // Snap to grapheme cluster boundary
-            offset = text.prev_grapheme_offset(offset + 1).unwrap();
+            offset = self.text().prev_grapheme_offset(offset + 1).unwrap();
         }
 
         // clamp to end of line
-        let next_line_offset = self.offset_of_line(text, line + 1);
+        let next_line_offset = self.offset_of_line(line + 1);
         if offset >= next_line_offset {
-            if let Some(prev) = text.prev_grapheme_offset(next_line_offset) {
+            if let Some(prev) = self.text().prev_grapheme_offset(next_line_offset) {
                 offset = prev;
             }
         }
