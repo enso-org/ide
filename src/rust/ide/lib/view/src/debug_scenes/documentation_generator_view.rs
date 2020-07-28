7d4206c73711053e1bbdd620f24c3280c404e7fa
BinarySoftware <mkmikolajek@icloud.com>
2020-07-28 19:04:46 +0200

    It does what I expect

--- src/rust/ide/lib/view/src/debug_scenes/documentation_generator_view.rs
+++ src/rust/ide/lib/view/src/debug_scenes/documentation_generator_view.rs
@@ -14,13 +14,53 @@ use nalgebra::Vector2;
 use nalgebra::Vector3;
 use wasm_bindgen::prelude::*;
 
-// TODO [MM]: Connect to doc parser
+fn generate_mock_doc() -> String {
+    let inp_only_doc = r#"DEPRECATED
+                             REMOVED - replaced by Foo Bar
+                             ADDED
+                             MODIFIED
+                             UPCOMING
+                             ALAMAKOTA a kot ma Ale
+                             This is a test of Enso Documentation Parser. This is a short synopsis.
+
+                             Here you can write the body of documentation. On top you can see tags
+                             added to this piece of code. You can customise your text with _Italic_
+                             ~Strikethrough~ or *Bold*. ~_*Combined*_~ is funny
+
+
+                             There are 3 kinds of sections
+                               - Important
+                               - Info
+                               - Example
+                                 * You can use example to add multiline code to your documentation
+
+                             ! Important
+                               Here is a small test of Important Section
+
+                             ? Info
+                               Here is a small test of Info Section
+
+                             > Example
+                               Here is a small test of Example Section
+                                   Import Foo
+                                   def Bar a
+                             "#;
+    let default_input = String::from(inp_only_doc);
+    let program = std::env::args().nth(1).unwrap_or(default_input);
+
+    let parser = parser::DocParser::new_or_panic();
+    let output = parser.generate_html_doc_pure(program);
+    let output_unwrapped = output.unwrap_or(String::from("<h1>hello EnsoGL</h1>"));
+    output_unwrapped
+}
 
 
 #[wasm_bindgen]
 #[allow(dead_code)]
 #[allow(clippy::many_single_char_names)]
 pub fn run_example_documentation_generator_view() {
+    let output_unwrapped = generate_mock_doc();
+
     web::forward_panic_hook_to_console();
     web::set_stdout();
     let world         = World::new(&web::get_html_element_by_id("root").unwrap());
@@ -34,7 +74,7 @@ pub fn run_example_documentation_generator_view() {
     let div = web::create_div();
     div.set_style_or_panic("width"  , "100%");
     div.set_style_or_panic("height" , "100%");
-    div.set_inner_html("<h1>hello baseGL</h1>");
+    div.set_inner_html(&output_unwrapped);
 
     let width  = screen.width;
     let height = screen.height;
@@ -45,9 +85,9 @@ pub fn run_example_documentation_generator_view() {
     let object     = DomSymbol::new(&div);
     dom_front_layer.manage(&object);
     world.add_child(&object);
-    let r          = (32.0) as u8;
-    let g          = (32.0) as u8;
-    let b          = (32.0) as u8;
+    let r          = (250.0) as u8;
+    let g          = (250.0) as u8;
+    let b          = (250.0) as u8;
     let color      = iformat!("rgb({r},{g},{b})");
     div.set_style_or_panic("background-color",color);
 
