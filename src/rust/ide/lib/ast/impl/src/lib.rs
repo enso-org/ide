c89902e281950bae9646875f746874aa8f49d6de
mwu <mwu-tow@gazeta.pl>
2020-08-15 02:51:39 +0200

    fixed test and the doc

--- src/rust/ide/lib/ast/impl/src/lib.rs
+++ src/rust/ide/lib/ast/impl/src/lib.rs
@@ -1177,7 +1177,10 @@ impl <T> Block<T> {
 }
 
 impl Block<Ast> {
-    /// Creates block from given line ASTs. There is no leading AST (it is orphan block).
+    /// Create a block from given line ASTs.
+    ///
+    /// If there are no tail lines, the first line will be "inline" and the whole block.
+    /// If there are tail lines, block will be leaded with a newline.
     pub fn from_lines(first_line:&Ast, tail_lines:&[Option<Ast>]) -> Block<Ast> {
         let ty          = BlockType::Discontinuous {};
         let indent      = 0;
