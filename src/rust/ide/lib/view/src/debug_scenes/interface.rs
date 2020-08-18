3e7637042bd65014767c382bf81039ea58728c2f
BinarySoftware <mkmikolajek@icloud.com>
2020-08-18 14:32:31 +0200

    gray-ish bg and lighter nodes

--- src/rust/ide/lib/view/src/debug_scenes/interface.rs
+++ src/rust/ide/lib/view/src/debug_scenes/interface.rs
@@ -79,6 +79,8 @@ fn init(app:&Application) {
 
     let mut dark = theme::Theme::new();
     dark.insert("application.background.color", color::Lcha::new(0.13,0.013,0.18,1.0));
+    dark.insert("application.text.color", color::Rgba::new(1.0,1.0,1.0,0.7));
+    dark.insert("project_name.text.transparent.color", color::Rgba::new(1.0,1.0,1.0,0.5));
     dark.insert("graph_editor.node.background.color", color::Lcha::new(0.2,0.013,0.18,1.0));
     dark.insert("graph_editor.node.selection.color", color::Lcha::new(0.72,0.5,0.22,1.0));
     dark.insert("graph_editor.node.selection.size", 7.0);
@@ -92,17 +94,18 @@ fn init(app:&Application) {
 
 
     let mut light = theme::Theme::new();
-    light.insert("application.background.color", color::Lcha::new(0.98,0.0,0.0,1.0));
-    light.insert("graph_editor.node.background.color", color::Lcha::new(0.78,0.013,0.18,0.9));
+    light.insert("application.background.color", color::Lcha::new(0.87,0.013,0.18,1.0));
+    light.insert("application.text.color", color::Rgba::new(0.0,0.0,0.0,0.7));
+    light.insert("project_name.text.transparent.color", color::Rgba::new(0.0,0.0,0.0,0.5));
+    light.insert("graph_editor.node.background.color", color::Lcha::new(0.98,0.013,0.18,0.9));
     light.insert("graph_editor.node.selection.color", color::Lcha::new(0.7,0.59,0.37,1.0));
     light.insert("graph_editor.node.selection.size", 7.0);
-    light.insert("graph_editor.visualization.background.color", color::Lcha::new(0.78,0.013,0.18,0.9));
+    light.insert("graph_editor.visualization.background.color", color::Lcha::new(0.98,0.013,0.18,0.9));
     light.insert("animation.duration", 0.5);
     light.insert("graph.node.shadow.color", 5.0);
     light.insert("graph.node.shadow.size", 5.0);
     // FIXME : this is not used, cursor uses it's DEFAULT_COLOR value.
     light.insert("mouse.pointer.color", color::Rgba::new(0.65,0.65,0.65,1.0));
-
 
     app.themes.register("light",light);
     app.themes.set_enabled(&["light"]);
