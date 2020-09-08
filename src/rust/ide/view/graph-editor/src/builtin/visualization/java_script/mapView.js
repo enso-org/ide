f22da22a343771ac99da0d63ee1d9f2e57036cc1
BinarySoftware <mkmikolajek@icloud.com>
2020-09-08 20:47:06 +0200

    presents data from input on scatterplot layer.

--- src/rust/ide/view/graph-editor/src/builtin/visualization/java_script/mapView.js
+++ src/rust/ide/view/graph-editor/src/builtin/visualization/java_script/mapView.js
@@ -38,56 +38,32 @@ class MapViewVisualization extends Visualization {
         mapElem.setAttributeNS(null,"style"    ,"width:" + width + "px;height: " + height + "px;");
         this.dom.appendChild(mapElem);
 
+        const parsedData = JSON.parse(data);
+
         const deckgl = new deck.DeckGL({
             container: 'map',
             mapboxApiAccessToken: 'pk.eyJ1IjoidWJlcmRhdGEiLCJhIjoiY2pudzRtaWloMDAzcTN2bzN1aXdxZHB5bSJ9.2bkj3IiRC8wj3jLThvDGdA',
-            mapStyle: 'mapbox://styles/mapbox/dark-v9',
+            mapStyle: parsedData.mapStyle || 'mapbox://styles/mapbox/dark-v9',
             initialViewState: {
-                longitude: -1.4157,
-                latitude: 52.2324,
-                zoom: 3,
-                minZoom: 5,
-                maxZoom: 15,
-                pitch: 40.5
+                longitude: parsedData.longitude || -1.4157,
+                latitude: parsedData.latitude || 52.2324,
+                zoom: parsedData.zoom || 3,
+                pitch: parsedData.pitch || 0
             },
-            controller: false
+            controller: parsedData.controller || false
         });
 
-        const dataaaa = d3.csv('https://raw.githubusercontent.com/uber-common/deck.gl-data/master/examples/3d-heatmap/heatmap-data.csv');
+        const layer = new deck.ScatterplotLayer({
+            id: 'scatterplot',
+            data: parsedData.data || [],
+            getColor: d => d.color,
+            getRadius: d => d.radius,
+            opacity: 1
+        });
 
-        console.log(data);
-
-        const COLOR_RANGE = [
-            [1, 152, 189],
-            [73, 227, 206],
-            [216, 254, 181],
-            [254, 237, 177],
-            [254, 173, 84],
-            [209, 55, 78]
-        ];
-
-        renderLayer();
-
-        function renderLayer () {
-            const hexagonLayer = new deck.HexagonLayer({
-                id: 'heatmap',
-                colorRange: COLOR_RANGE,
-                data: dataaaa,
-                elevationRange: [0, 1000],
-                elevationScale: 250,
-                extruded: true,
-                getPosition: d => [Number(d.lng), Number(d.lat)],
-                opacity: 1
-            });
-
-            deckgl.setProps({
-                layers: [hexagonLayer]
-            });
-        }
-
-        // data.forEach(data => {
-        //     ...
-        // });
+        deckgl.setProps({
+            layers: [layer]
+        });
     }
 
     setSize(size) {
