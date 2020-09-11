e30dcff5cfe460c0732ee406c54e059750615d6a
BinarySoftware <mkmikolajek@icloud.com>
2020-09-11 12:20:32 +0200

    move

--- src/rust/ide/src/view/project.rs
+++ src/rust/ide/src/view/project.rs
@@ -10,15 +10,14 @@ use crate::view::layout::ViewLayout;
 use ensogl::application::Application;
 use ensogl::control::callback;
 use ensogl::control::io::keyboard::listener::KeyboardFrpBindings;
-use ensogl::data::color;
 use ensogl::display::navigation::navigator::Navigator;
 use ensogl::display::shape::text::glyph::font;
-use ensogl::display::style::theme;
 use ensogl::system::web;
 use enso_frp::io::keyboard::Keyboard;
 use enso_frp::io::keyboard;
 use enso_shapely::shared;
 use ide_view::graph_editor;
+use ide_view::theme;
 use nalgebra::Vector2;
 
 
@@ -111,7 +110,7 @@ impl ProjectView {
         let camera            = scene.camera();
         let navigator         = Navigator::new(&scene,&camera);
         Self::setup_components(&application);
-        Self::setup_theme(&application);
+        theme::setup(&application);
         let _world = &application.display;
         // graph::register_shapes(&world);
         let keyboard                 = Keyboard::default();
@@ -143,68 +142,6 @@ impl ProjectView {
 
     fn setup_components(app:&Application) {
         app.views.register::<graph_editor::GraphEditor>();
-    }
-
-    fn setup_theme(app:&Application) {
-        let mut dark = theme::Theme::new();
-        dark.insert("application.background.color", color::Lcha::new(0.13,0.013,0.18,1.0));
-        dark.insert("application.text.color", color::Lcha::new(1.0,0.0,0.0,0.7));
-        dark.insert("text.selection.color", color::Lcha::new(0.7,0.0,0.125,0.7));
-
-        dark.insert("graph_editor.node.background.color", color::Lcha::new(0.2,0.013,0.18,1.0));
-        dark.insert("graph_editor.node.selection.color", color::Lcha::new(0.72,0.5,0.22,1.0));
-        dark.insert("graph_editor.node.selection.size", 7.0);
-        dark.insert("graph_editor.visualization.background.color", color::Lcha::new(0.2,0.013,0.18,1.0));
-
-        dark.insert("breadcrumbs.full.color", color::Lcha::new(1.0,0.0,0.0,0.7));
-        dark.insert("breadcrumbs.transparent.color", color::Lcha::new(1.0,0.0,0.0,0.4));
-        dark.insert("breadcrumbs.selected.color", color::Lcha::new(1.0,0.0,0.0,0.6));
-        dark.insert("breadcrumbs.left.deselected.color", color::Lcha::new(1.0,0.0,0.0,0.6));
-        dark.insert("breadcrumbs.right.deselected.color", color::Lcha::new(1.0,0.0,0.0,0.2));
-        dark.insert("breadcrumbs.hover.color", color::Lcha::new(1.0,0.0,0.0,0.6));
-
-        dark.insert("list_view.background.color", color::Lcha::new(0.2,0.013,0.18,1.0));
-        dark.insert("list_view.highlight.color", color::Lcha::new(0.72,0.5,0.22,1.0));
-
-        dark.insert("edge.split_color_lightness_factor", 0.2);
-        dark.insert("edge.split_color_chroma_factor", 1.0);
-
-        dark.insert("type.missing.color", color::Lcha::new(0.5,0.0,0.0,1.0));
-        dark.insert("type.color_luminance", 0.5);
-        dark.insert("type.color_chroma", 0.8);
-
-        app.themes.register("dark",dark);
-
-
-        let mut light = theme::Theme::new();
-        light.insert("application.background.color", color::Lcha::new(0.96,0.013,0.18,1.0));
-        light.insert("application.text.color", color::Lcha::new(0.0,0.0,0.0,0.7));
-        light.insert("text.selection.color", color::Lcha::new(0.7,0.0,0.125,0.7));
-
-        light.insert("graph_editor.node.background.color", color::Lcha::new(0.98,0.013,0.18,1.0));
-        light.insert("graph_editor.node.selection.color", color::Lcha::new(0.83,0.58,0.436,1.0));
-        light.insert("graph_editor.node.selection.size", 7.0);
-        light.insert("graph_editor.visualization.background.color", color::Lcha::new(0.98,0.013,0.18,1.0));
-
-        light.insert("breadcrumbs.full.color", color::Lcha::new(0.0,0.0,0.0,0.7));
-        light.insert("breadcrumbs.transparent.color", color::Lcha::new(0.0,0.0,0.0,0.4));
-        light.insert("breadcrumbs.selected.color", color::Lcha::new(0.0,0.0,0.0,0.6));
-        light.insert("breadcrumbs.left.deselected.color", color::Lcha::new(0.0,0.0,0.0,0.6));
-        light.insert("breadcrumbs.right.deselected.color", color::Lcha::new(0.0,0.0,0.0,0.2));
-        light.insert("breadcrumbs.hover.color", color::Lcha::new(0.0,0.0,0.0,0.6));
-
-        light.insert("list_view.background.color", color::Lcha::new(0.98,0.013,0.18,1.0));
-        light.insert("list_view.highlight.color", color::Lcha::new(0.55,0.65,0.79,1.0));
-
-        light.insert("edge.split_color_lightness_factor", 1.2);
-        light.insert("edge.split_color_chroma_factor", 0.8);
-
-        light.insert("type.missing.color", color::Lcha::new(0.8,0.0,0.0,1.0));
-        light.insert("type.color_luminance", 0.8);
-        light.insert("type.color_chroma", 0.6);
-
-        app.themes.register("light",light);
-        app.themes.set_enabled(&["light"]);
     }
 
     /// Forgets ProjectView, so it won't get dropped when it goes out of scope.
--- src/rust/ide/view/src/debug_scenes/interface.rs
+++ src/rust/ide/view/src/debug_scenes/interface.rs
@@ -8,14 +8,13 @@ use crate::prelude::*;
 
 use crate::graph_editor::GraphEditor;
 use crate::graph_editor::Type;
+use crate::theme;
 
 use enso_frp as frp;
 use ensogl::display::navigation::navigator::Navigator;
 use ensogl::system::web;
 use ensogl::application::Application;
 use ensogl::display::object::ObjectOps;
-use ensogl::display::style::theme;
-use ensogl::data::color;
 use wasm_bindgen::prelude::*;
 
 
@@ -77,65 +76,7 @@ impl DummyTypeGenerator {
 
 fn init(app:&Application) {
 
-    let mut dark = theme::Theme::new();
-    dark.insert("application.background.color", color::Lcha::new(0.13,0.013,0.18,1.0));
-    dark.insert("application.text.color", color::Lcha::new(1.0,1.0,1.0,0.7));
-    dark.insert("text.selection.color", color::Lcha::new(0.7,0.0,0.125,0.7));
-
-    dark.insert("graph_editor.node.background.color", color::Lcha::new(0.2,0.013,0.18,1.0));
-    dark.insert("graph_editor.node.selection.color", color::Lcha::new(0.72,0.5,0.22,1.0));
-    dark.insert("graph_editor.node.selection.size", 7.0);
-    dark.insert("graph_editor.visualization.background.color", color::Lcha::new(0.2,0.013,0.18,1.0));
-
-    dark.insert("breadcrumbs.full.color", color::Lcha::new(1.0,1.0,1.0,0.7));
-    dark.insert("breadcrumbs.transparent.color", color::Lcha::new(1.0,1.0,1.0,0.4));
-    dark.insert("breadcrumbs.selected.color", color::Lcha::new(1.0,1.0,1.0,0.6));
-    dark.insert("breadcrumbs.left.deselected.color", color::Lcha::new(1.0,1.0,1.0,0.6));
-    dark.insert("breadcrumbs.right.deselected.color", color::Lcha::new(1.0,1.0,1.0,0.2));
-    dark.insert("breadcrumbs.hover.color", color::Lcha::new(1.0,1.0,1.0,0.6));
-
-    dark.insert("list_view.background.color", color::Lcha::new(0.2,0.013,0.18,1.0));
-    dark.insert("list_view.highlight.color", color::Lcha::new(0.72,0.5,0.22,1.0));
-
-    dark.insert("edge.split_color_lightness_factor", 0.2);
-    dark.insert("edge.split_color_chroma_factor", 1.0);
-
-    dark.insert("type.missing.color", color::Lcha::new(0.5, 0.0, 0.0, 1.0));
-    dark.insert("type.color_luminance", 0.5);
-    dark.insert("type.color_chroma", 0.8);
-
-    app.themes.register("dark",dark);
-
-
-    let mut light = theme::Theme::new();
-    light.insert("application.background.color", color::Lcha::new(0.96,0.013,0.18,1.0));
-    light.insert("application.text.color", color::Lcha::new(0.0,0.0,0.0,0.7));
-    light.insert("text.selection.color", color::Lcha::new(0.7,0.0,0.125,0.7));
-
-    light.insert("graph_editor.node.background.color", color::Lcha::new(0.98,0.013,0.18,1.0));
-    light.insert("graph_editor.node.selection.color", color::Lcha::new(0.83,0.58,0.436,1.0));
-    light.insert("graph_editor.node.selection.size", 7.0);
-    light.insert("graph_editor.visualization.background.color", color::Lcha::new(0.98,0.013,0.18,1.0));
-
-    light.insert("breadcrumbs.full.color", color::Lcha::new(0.0,0.0,0.0,0.7));
-    light.insert("breadcrumbs.transparent.color", color::Lcha::new(0.0,0.0,0.0,0.4));
-    light.insert("breadcrumbs.selected.color", color::Lcha::new(0.0,0.0,0.0,0.6));
-    light.insert("breadcrumbs.left.deselected.color", color::Lcha::new(0.0,0.0,0.0,0.6));
-    light.insert("breadcrumbs.right.deselected.color", color::Lcha::new(0.0,0.0,0.0,0.2));
-    light.insert("breadcrumbs.hover.color", color::Lcha::new(0.0,0.0,0.0,0.6));
-
-    light.insert("list_view.background.color", color::Lcha::new(0.98,0.013,0.18,1.0));
-    light.insert("list_view.highlight.color", color::Lcha::new(0.55,0.65,0.79,1.0));
-
-    light.insert("edge.split_color_lightness_factor", 1.2);
-    light.insert("edge.split_color_chroma_factor", 0.8);
-
-    light.insert("type.missing.color", color::Lcha::new(0.8, 0.0, 0.0, 1.0));
-    light.insert("type.color_luminance", 0.8);
-    light.insert("type.color_chroma", 0.6);
-
-    app.themes.register("light",light);
-    app.themes.set_enabled(&["light"]);
+    theme::setup(&app);
 
     let _bg = app.display.scene().style_sheet.var("application.background.color");
 
