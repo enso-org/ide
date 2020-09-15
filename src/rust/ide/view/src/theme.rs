9b1c15390d21cfed68871d1c6c4005dc8a9a3401
Adam Obuchowicz <adam.obuchowicz@enso.org>
2020-09-15 15:30:04 +0200

    A little redesign

--- src/rust/ide/view/src/theme.rs
+++ src/rust/ide/view/src/theme.rs
@@ -46,13 +46,11 @@ macro_rules! _define_theme_modules {
     };
 }
 
-/// Used to define theme.
+/// Used to define default theme.
 #[macro_export]
-macro_rules! define_theme {
-    (($app:ident , $name:ident) $($t:tt)*) => {
-        let mut $name = theme::Theme::new();
-        _define_theme_literals!([$name] $($t)*);
-        $app.themes.register(stringify!($name),$name);
+macro_rules! define_default_theme {
+    ($name:ident $($t:tt)*) => {
+        define_theme!($name, $($t)*)
 
         if cfg!(not(_Theme_Vars_)) {
             #[cfg(_Theme_Vars_)]
@@ -63,158 +61,168 @@ macro_rules! define_theme {
     };
 }
 
-/// Used to set up themes for the application.
-pub fn setup(app:&Application) {
+/// Used to define default theme.
+#[macro_export]
+macro_rules! define_theme {
+    ($name:ident $($t:tt)*) => {
+        #[allow(missing_docs)]
+        pub mod $name {
+            /// Setup the $name theme in application.
+            pub fn setup(app:&Application) {
+                let mut $name = theme::Theme::new();
+                _define_theme_literals!([$name] $($t)*);
+                app.themes.register(stringify!($name),$name);
+            }
+        }
+    };
+}
 
-    define_theme! { (app,dark)
-        application {
-            background {
-                color = color::Lcha::new(0.13,0.013,0.18,1.0)
-            }
-            text {
-                color = color::Lcha::new(1.0,0.0,0.0,0.7);
-                selection {
-                    color = color::Lcha::new(0.7,0.0,0.125,0.7)
-                }
+define_default_theme! { dark
+    application {
+        background {
+            color = color::Lcha::new(0.13,0.013,0.18,1.0)
+        }
+        text {
+            color = color::Lcha::new(1.0,0.0,0.0,0.7);
+            selection {
+                color = color::Lcha::new(0.7,0.0,0.125,0.7)
             }
         }
-        graph_editor {
-            node {
-                background {
-                    color = color::Lcha::new(0.2,0.013,0.18,1.0)
-                }
-                selection {
-                    color = color::Lcha::new(0.72,0.5,0.22,1.0);
-                    size = 7.0
-                }
-            }
-            visualization {
-                background {
-                    color = color::Lcha::new(0.2,0.013,0.18,1.0)
-                }
-            }
-        }
-        breadcrumbs {
-            full {
-                color = color::Lcha::new(1.0,0.0,0.0,0.7)
-            }
-            transparent {
-                color = color::Lcha::new(1.0,0.0,0.0,0.4)
-            }
-            selected {
-                color = color::Lcha::new(1.0,0.0,0.0,0.6)
-            }
-            deselected{
-                left {
-                    color = color::Lcha::new(1.0,0.0,0.0,0.6)
-                }
-                right {
-                    color = color::Lcha::new(1.0,0.0,0.0,0.2)
-                }
-            }
-            hover {
-                color = color::Lcha::new(1.0,0.0,0.0,0.6)
-            }
-        }
-        list_view {
+    }
+    graph_editor {
+        node {
             background {
                 color = color::Lcha::new(0.2,0.013,0.18,1.0)
             }
-            highlight {
-                color = color::Lcha::new(0.72,0.5,0.22,1.0)
+            selection {
+                color = color::Lcha::new(0.72,0.5,0.22,1.0);
+                size = 7.0
             }
         }
-        edge {
-            split_color {
-                lightness_factor = 0.2;
-                chroma_factor = 1.0
-            }
-        }
-        _type {
-            missing {
-                color = color::Lcha::new(0.5,0.0,0.0,1.0)
-            }
-            color {
-                luminance = 0.5;
-                chroma = 0.8
+        visualization {
+            background {
+                color = color::Lcha::new(0.2,0.013,0.18,1.0)
             }
         }
     }
+    breadcrumbs {
+        full {
+            color = color::Lcha::new(1.0,0.0,0.0,0.7)
+        }
+        transparent {
+            color = color::Lcha::new(1.0,0.0,0.0,0.4)
+        }
+        selected {
+            color = color::Lcha::new(1.0,0.0,0.0,0.6)
+        }
+        deselected{
+            left {
+                color = color::Lcha::new(1.0,0.0,0.0,0.6)
+            }
+            right {
+                color = color::Lcha::new(1.0,0.0,0.0,0.2)
+            }
+        }
+        hover {
+            color = color::Lcha::new(1.0,0.0,0.0,0.6)
+        }
+    }
+    list_view {
+        background {
+            color = color::Lcha::new(0.2,0.013,0.18,1.0)
+        }
+        highlight {
+            color = color::Lcha::new(0.72,0.5,0.22,1.0)
+        }
+    }
+    edge {
+        split_color {
+            lightness_factor = 0.2;
+            chroma_factor = 1.0
+        }
+    }
+    _type {
+        missing {
+            color = color::Lcha::new(0.5,0.0,0.0,1.0)
+        }
+        color {
+            luminance = 0.5;
+            chroma = 0.8
+        }
+    }
+}
 
-    define_theme! { (app,light)
-        application {
-            background {
-                color = color::Lcha::new(0.96,0.013,0.18,1.0)
-            }
-            text {
-                color = color::Lcha::new(0.0,0.0,0.0,0.7);
-                selection {
-                    color = color::Lcha::new(0.7,0.0,0.125,0.7)
-                }
+define_theme! { light
+    application {
+        background {
+            color = color::Lcha::new(0.96,0.013,0.18,1.0)
+        }
+        text {
+            color = color::Lcha::new(0.0,0.0,0.0,0.7);
+            selection {
+                color = color::Lcha::new(0.7,0.0,0.125,0.7)
             }
         }
-        graph_editor {
-            node {
-                background {
-                    color = color::Lcha::new(0.98,0.013,0.18,1.0)
-                }
-                selection {
-                    color = color::Lcha::new(0.83,0.58,0.436,1.0);
-                    size = 7.0
-                }
-            }
-            visualization {
-                background {
-                    color = color::Lcha::new(0.98,0.013,0.18,1.0)
-                }
-            }
-        }
-        breadcrumbs {
-            full {
-                color = color::Lcha::new(0.0,0.0,0.0,0.7)
-            }
-            transparent {
-                color = color::Lcha::new(0.0,0.0,0.0,0.4)
-            }
-            selected {
-                color = color::Lcha::new(0.0,0.0,0.0,0.6)
-            }
-            deselected{
-                left {
-                    color = color::Lcha::new(0.0,0.0,0.0,0.6)
-                }
-                right {
-                    color = color::Lcha::new(0.0,0.0,0.0,0.2)
-                }
-            }
-            hover {
-                color = color::Lcha::new(0.0,0.0,0.0,0.6)
-            }
-        }
-        list_view {
+    }
+    graph_editor {
+        node {
             background {
                 color = color::Lcha::new(0.98,0.013,0.18,1.0)
             }
-            highlight {
-                color = color::Lcha::new(0.55,0.65,0.79,1.0)
+            selection {
+                color = color::Lcha::new(0.83,0.58,0.436,1.0);
+                size = 7.0
             }
         }
-        edge {
-            split_color {
-                lightness_factor = 1.2;
-                chroma_factor = 0.8
-            }
-        }
-        _type {
-            missing {
-                color = color::Lcha::new(0.8,0.0,0.0,1.0)
-            }
-            color {
-                luminance = 0.8;
-                chroma = 0.6
+        visualization {
+            background {
+                color = color::Lcha::new(0.98,0.013,0.18,1.0)
             }
         }
     }
-
-    app.themes.set_enabled(&["light"]);
+    breadcrumbs {
+        full {
+            color = color::Lcha::new(0.0,0.0,0.0,0.7)
+        }
+        transparent {
+            color = color::Lcha::new(0.0,0.0,0.0,0.4)
+        }
+        selected {
+            color = color::Lcha::new(0.0,0.0,0.0,0.6)
+        }
+        deselected{
+            left {
+                color = color::Lcha::new(0.0,0.0,0.0,0.6)
+            }
+            right {
+                color = color::Lcha::new(0.0,0.0,0.0,0.2)
+            }
+        }
+        hover {
+            color = color::Lcha::new(0.0,0.0,0.0,0.6)
+        }
+    }
+    list_view {
+        background {
+            color = color::Lcha::new(0.98,0.013,0.18,1.0)
+        }
+        highlight {
+            color = color::Lcha::new(0.55,0.65,0.79,1.0)
+        }
+    }
+    edge {
+        split_color {
+            lightness_factor = 1.2;
+            chroma_factor = 0.8
+        }
+    }
+    _type {
+        missing {
+            color = color::Lcha::new(0.8,0.0,0.0,1.0)
+        }
+        color {
+            luminance = 0.8;
+            chroma = 0.6
+        }
+    }
 }
