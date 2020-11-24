63ed9fdfb0d6b255e25f7c614b14ae32dafa6a03
Michael Mauderer <michael.mauderer@enso.org>
2020-11-24 12:38:54 +0100

    style: Reformat JS to conform to prettier.

--- src/rust/ide/view/graph-editor/src/builtin/visualization/java_script/loading.js
+++ src/rust/ide/view/graph-editor/src/builtin/visualization/java_script/loading.js
@@ -2,23 +2,23 @@
  * Add script to the DOM.
  */
 function loadScript(url, onload) {
-  let script = document.createElement("script");
-  script.onload = onload;
-  script.src = url;
+    let script = document.createElement('script')
+    script.onload = onload
+    script.src = url
 
-  document.head.appendChild(script);
+    document.head.appendChild(script)
 }
 
 /**
  * Add css style file to the DOM.
  */
 function loadStyle(url, onload) {
-  let style = document.createElement("link");
-  style.onload = onload;
-  style.href = url;
-  style.rel = "stylesheet";
-  style.media = "screen";
-  style.type = "text/css";
+    let style = document.createElement('link')
+    style.onload = onload
+    style.href = url
+    style.rel = 'stylesheet'
+    style.media = 'screen'
+    style.type = 'text/css'
 
-  document.head.appendChild(style);
+    document.head.appendChild(style)
 }
