8f4c38e8ac5e2e795d2782032a9f65af2b4fc22b
BinarySoftware <mkmikolajek@icloud.com>
2021-01-08 18:51:24 +0100

    bump parser

--- src/rust/ide/lib/parser/build.rs
+++ src/rust/ide/lib/parser/build.rs
@@ -27,7 +27,7 @@ const PARSER_PATH: &str = "./pkg/scala-parser.js";
 /// Commit from `enso` repository that will be used to obtain parser from.
 /// FIXME: Using commit not from Engine's main branch but from a backported branch as the AST in
 ///        this project hasn't been updated to conform to latest scala parser (#732).
-const PARSER_COMMIT: &str = "9d848fc59bfbab8aa202da71dc899759817dff0f";
+const PARSER_COMMIT: &str = "92514b8567d51ce571e03be1f160f82204f469bc";
 
 /// Magic code that needs to be prepended to ScalaJS generated parser due to:
 /// https://github.com/scala-js/scala-js/issues/3677/
