5b57c4570c103ab34213e40a3c5f753593ad8321
Adam Obuchowicz <adam.obuchowicz@enso.org>
2021-01-12 17:00:36 +0100

    Fix output edge coloring

--- src/rust/ide/view/graph-editor/src/lib.rs
+++ src/rust/ide/view/graph-editor/src/lib.rs
@@ -10,6 +10,7 @@
 #![feature(entry_insert)]
 #![feature(fn_traits)]
 #![feature(overlapping_marker_traits)]
+#![feature(option_result_contains)]
 #![feature(specialization)]
 #![feature(trait_alias)]
 #![feature(type_alias_impl_trait)]
@@ -1528,6 +1529,9 @@ impl GraphEditorModel {
     fn set_node_expression_usage_type(&self, node_id:impl Into<NodeId>, ast_id:ast::Id, maybe_type:Option<Type>) {
         let node_id  = node_id.into();
         if let Some(node) = self.nodes.get_cloned_ref(&node_id) {
+            if node.view.model.output.whole_expr_id().contains(&ast_id) {
+                node.view.model.output.set_expression_usage_type(span_tree::Crumbs::default(), maybe_type.clone());
+            }
             let crumbs = node.view.model.get_crumbs_by_id(ast_id);
             if let Some(crumbs) = crumbs {
                 node.view.frp.set_expression_usage_type.emit((crumbs,maybe_type));
