5581a42d5f16bdf42c6bc6f16d31c7e5fed94154
BinarySoftware <mkmikolajek@icloud.com>
2021-02-05 16:45:58 +0100

    rm

--- src/rust/ide/view/src/documentation.rs
+++ src/rust/ide/view/src/documentation.rs
@@ -143,7 +143,6 @@ impl Model {
         } else {
             let parser        = parser::DocParser::new()?;
             let mut processed = string.to_string();
-            println!("DOC IN:\n{:?}",processed);
             let lines = processed.lines().collect::<Vec<&str>>();
             if lines.len() > 1 {
                 let mut indent = 0;
@@ -153,13 +152,11 @@ impl Model {
                 }
                 processed = format!{"{}{}",String::from(" ").repeat(indent),processed};
             }
-            println!("DOC FIXED:\n{:?}",processed);
             let output = match input_type {
                 InputFormat::AST       => parser.generate_html_docs(processed),
                 InputFormat::Docstring => parser.generate_html_doc_pure(processed),
             };
             let output = output?;
-            println!("DOC OUT:\n{:?}",output);
             Ok( if output.is_empty() { PLACEHOLDER_STR.into() } else { output } )
         }
     }
