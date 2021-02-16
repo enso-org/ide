899e8698e27d1025d6b2eacee3c8c2c654c8148f
BinarySoftware <mkmikolajek@icloud.com>
2021-02-16 10:08:40 +0100

    [ci no changelog needed] lint

--- src/rust/ide/view/graph-editor/src/builtin/visualization/java_script/geoMap.js
+++ src/rust/ide/view/graph-editor/src/builtin/visualization/java_script/geoMap.js
@@ -31,8 +31,6 @@ const DEFAULT_MAP_ZOOM = 11
 const DARK_ACCENT_COLOR = [222, 162, 47]
 const LIGHT_ACCENT_COLOR = [1, 234, 146]
 
-
-
 // =====================================
 // === Script & Style Initialisation ===
 // =====================================
