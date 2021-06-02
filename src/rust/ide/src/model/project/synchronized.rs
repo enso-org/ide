1014e5a68e4405580799842fb119fea6ecc4615e
Adam Obuchowicz <adam.obuchowicz@enso.org>
2021-06-02 09:43:32 +0200

    Uploading file in project controller.

--- src/rust/ide/src/model/project/synchronized.rs
+++ src/rust/ide/src/model/project/synchronized.rs
@@ -5,6 +5,7 @@ use crate::prelude::*;
 use crate::model::execution_context::VisualizationUpdateData;
 use crate::model::execution_context;
 use crate::model::module;
+use crate::model::project::upload;
 use crate::model::SuggestionDatabase;
 use crate::model::traits::*;
 use crate::notification;
@@ -15,6 +16,7 @@ use enso_protocol::binary::message::VisualisationContext;
 use enso_protocol::language_server;
 use enso_protocol::language_server::CapabilityRegistration;
 use enso_protocol::language_server::MethodPointer;
+use enso_protocol::language_server::Path as FilePath;
 use enso_protocol::project_manager;
 use enso_protocol::project_manager::MissingComponentAction;
 use flo_stream::Subscriber;
@@ -401,6 +403,14 @@ impl model::project::API for Project {
 
     fn subscribe(&self) -> Subscriber<model::project::Notification> {
         self.notifications.subscribe()
+    }
+
+    fn upload_file
+    (&self, path:FilePath, size:usize, data_provider:Box<dyn upload::DataProvider>)
+     -> StaticBoxStream<upload::Notification> {
+        let connection = self.language_server_bin.clone_ref();
+        let process    = upload::FileUploadProcess::new(&self.logger,connection,path);
+        process.start(data_provider).boxed_local()
     }
 }
 
--- src/rust/ide/src/model/project/upload.rs
+++ src/rust/ide/src/model/project/upload.rs
@@ -7,7 +7,7 @@ use enso_protocol::language_server::Path;
 use futures::SinkExt;
 use ensogl::application::shortcut::Condition::Not; //TODO maybe from the binary protocol?
 
-#[derive(Clone,Debug,Default)]
+#[derive(Clone,Debug)]
 pub enum Notification {
     UploadProgress {
         file_size      : usize,
@@ -16,7 +16,19 @@ pub enum Notification {
     Error {msg:ImString}
 }
 
+pub trait DataProvider {
+    fn next_chunk(&mut self) -> BoxFuture<FallibleResult<Option<Vec<u8>>>>;
+}
 
+impl<T:DataProvider + ?Sized> DataProvider for Box<T> {
+    fn next_chunk(&mut self) -> BoxFuture<FallibleResult<Option<Vec<u8>>>> {
+        // We reassign to type-constrained deref to be sure we won't fall the infinite recursion.
+        let deref:&mut T = &mut **self;
+        deref.next_chunk()
+    }
+}
+
+#[derive(Clone,Debug)]
 pub struct FileUploadProcess {
     logger         : Logger,
     connection     : Rc<Connection>,
@@ -26,7 +38,6 @@ pub struct FileUploadProcess {
     // TODO checksums
 }
 
-
 impl FileUploadProcess {
     pub fn new(parent:impl AnyLogger, connection:Rc<Connection>, path:Path) -> Self {
         let logger         = Logger::sub(parent,"FileUploadProcess");
@@ -35,32 +46,34 @@ impl FileUploadProcess {
         Self{logger,connection,path,size,bytes_uploaded}
     }
 
-    pub fn start<ChunkFuture>
-    (mut self, data_provider:impl FnMut() -> ChunkFuture) -> impl Stream<Item=Notification>
-    where ChunkFuture : Future<Output=FallibleResult<Some<Vec<u8>>>> {
+    pub fn start
+    (mut self, mut data_provider:impl DataProvider + 'static) -> impl Stream<Item=Notification> {
         let (mut notification_in,notification_out) = futures::channel::mpsc::channel(5); //TODO ugly constant
         executor::global::spawn(async move {
             loop {
-                match data_provider().await {
+                match data_provider.next_chunk().await {
                     Ok(Some(data)) => {
                         info!(self.logger, "Received chunk of {self.path} of size {data.len()}");
-                        self.connection.write_file(&path,&data); // TODO replace with write bytes.
+                        self.connection.write_file(&self.path,&data); // TODO replace with write bytes.
                         self.bytes_uploaded += data.len();
-                        let notification = Notification::UploadProgress {file_size,bytes_uploaded};
+                        let notification = Notification::UploadProgress {
+                            file_size      : self.size,
+                            bytes_uploaded : self.bytes_uploaded
+                        };
                         notification_in.send(notification).await;
                     },
                     Ok(None) => {
-                        if self.bytes_uploaded != self.file_size {
-                            error!(self.logger, "The promised file size ({self.file_size}) \
-                                and uploaded data length ({self.bytes_uploaded}) do not match. \
-                                Leaving as much data as received");
+                        if self.bytes_uploaded != self.size {
+                            error!(self.logger, "The promised file size ({self.size}) and uploaded \
+                                data length ({self.bytes_uploaded}) do not match. Leaving as much \
+                                data as received.");
                         }
                         break;
                     }
                     Err(err) => {
                         error!(self.logger, "Error while retrieving next chunk for {self.path}: \
                             {err:?}");
-                        let msg = err.msg.into();
+                        let msg = err.to_string().into();
                         notification_in.send(Notification::Error {msg}).await;
                         break;
                     }
